fc05b8c449901ecc99e9e5ba0952f03d8aaefb30
Revi1337 <david122123@gmail.com>
2024-04-06 01:20:29 +0900

    initial commit

---
+++ src/main/java/revi1337/onsquadserver/OnsquadServerApplication.java
@@ -0,0 +1,13 @@
+package revi1337.onsquadserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnsquadServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnsquadServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/revi1337/onsquadserver/OnsquadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package revi1337.onsquadserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnsquadServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
