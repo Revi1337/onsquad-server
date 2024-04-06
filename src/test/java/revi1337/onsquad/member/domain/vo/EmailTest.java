cccb44d45cdb0f965e647e37f1e4c62157491137
Revi1337 <david122123@gmail.com>
2024-04-06 16:02:01 +0900

    test: 멤버 도메인 테스트 코드 작성

---
+++ src/test/java/revi1337/onsquad/member/domain/vo/EmailTest.java
@@ -0,0 +1,45 @@
+package revi1337.onsquad.member.domain.vo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@DisplayName("Email vo 테스트")
+class EmailTest {
+
+    @DisplayName("이메일은 필수 명시사항이어야 한다.")
+    @Test
+    public void makeEmailWhenNull() {
+        assertThatThrownBy(() -> new Email(null))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("이메일은 null 일 수 없습니다.");
+    }
+
+    @DisplayName("정상적인 이메일은 Email 정규식이 통과되어야 한다.")
+    @Test
+    public void makeEmailWithFormat() {
+        // given
+        String value = "test@test.com";
+
+        // when
+        Email email = new Email(value);
+
+        // then
+        assertThat(email).isNotNull();
+    }
+
+    @DisplayName("정상적인 이메일은 Email 정규식이 통과되어야 한다.")
+    @Test
+    public void makeEmailWithFormat2() {
+        // given
+        String value = "test!!test.com.com";
+
+        // when & then
+        assertThatThrownBy(() -> new Email(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이메일 형식이 일치하지 않습니다.");
+    }
+
+}
---
+++ src/test/java/revi1337/onsquad/member/domain/vo/NicknameTest.java
@@ -0,0 +1,57 @@
+package revi1337.onsquad.member.domain.vo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@DisplayName("Nickname vo 테스트")
+class NicknameTest {
+
+    @DisplayName("닉네임은 필수 명시사항이어야 한다.")
+    @Test
+    public void makeNicknameWhenNull() {
+        assertThatThrownBy(() -> new Nickname(null))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("닉네임은 null 일 수 없습니다.");
+    }
+
+    @DisplayName("닉네임은 2자 이상 8자이하여야 한다.")
+    @Test
+    public void makeNicknameWithLength() {
+        // given
+        String value = "12";
+
+        // when
+        Nickname nickname = new Nickname(value);
+
+        // then
+        assertThat(nickname).isNotNull();
+    }
+
+    @DisplayName("닉네임은 2자 미만이면 안된다.")
+    @Test
+    public void makeNicknameWithLength2() {
+        // given
+        String value = "1";
+
+        // when & then
+        assertThatThrownBy(() -> new Nickname(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("닉네임은 2자 이상 8자 이하여야 합니다.");
+    }
+
+    @DisplayName("닉네임은 8자 초과이면 안된다.")
+    @Test
+    public void makeNicknameWithLength3() {
+        // given
+        String value = "123456789";
+
+        // when & then
+        assertThatThrownBy(() -> new Nickname(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("닉네임은 2자 이상 8자 이하여야 합니다.");
+    }
+
+}
