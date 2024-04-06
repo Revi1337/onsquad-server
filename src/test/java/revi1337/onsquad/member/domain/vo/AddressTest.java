6dfaa32ce256a34623d0cc07e0b3d61271201daa
Revi1337 <david122123@gmail.com>
2024-04-06 16:12:52 +0900

    test: 멤버 도메인 테스트 코드 작성

---
+++ src/test/java/revi1337/onsquad/member/domain/vo/AddressTest.java
@@ -0,0 +1,19 @@
+package revi1337.onsquad.member.domain.vo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@DisplayName("Address vo 테스트")
+class AddressTest {
+
+    @DisplayName("주소는 필수 명시사항이어야 한다.")
+    @Test
+    public void makeEmailWhenNull() {
+        assertThatThrownBy(() -> new Address(null))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("주소는 null 일 수 없습니다.");
+    }
+
+}
