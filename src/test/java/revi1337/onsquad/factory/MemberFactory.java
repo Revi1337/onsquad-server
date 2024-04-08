1ed56eed66ee1dc0ed4010ae405fc674425d15aa
Revi1337 <david122123@gmail.com>
2024-04-09 02:11:27 +0900

    test: 영속성 객체를 반환하는 팩토리클래스 작성

---
+++ src/test/java/revi1337/onsquad/factory/MemberFactory.java
@@ -0,0 +1,39 @@
+package revi1337.onsquad.factory;
+
+import revi1337.onsquad.member.domain.Member;
+import revi1337.onsquad.member.domain.vo.Address;
+import revi1337.onsquad.member.domain.vo.Email;
+import revi1337.onsquad.member.domain.vo.Nickname;
+import revi1337.onsquad.member.domain.vo.UserType;
+
+public class MemberFactory {
+
+    public static final Nickname NICKNAME = new Nickname("nickname");
+    public static final Email EMAIL = new Email("test@email.com");
+    public static final Address ADDRESS = new Address("어딘가");
+    public static final UserType USER_TYPE = UserType.GENERAL;
+
+    public static Member withNickname(Nickname nickname) {
+        return defaultMember().nickname(nickname).build();
+    }
+
+    public static Member withEmail(Email email) {
+        return defaultMember().email(email).build();
+    }
+
+    public static Member withAddress(Address address) {
+        return defaultMember().address(address).build();
+    }
+
+    public static Member withUserType(UserType userType) {
+        return defaultMember().userType(userType).build();
+    }
+
+    public static Member.MemberBuilder defaultMember() {
+        return Member.builder()
+                .nickname(NICKNAME)
+                .address(ADDRESS)
+                .email(EMAIL)
+                .userType(USER_TYPE);
+    }
+}
