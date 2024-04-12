9f9281c01666ee73bc5e32eb191b423c4aa007c6
Revi1337 <david122123@gmail.com>
2024-04-13 05:55:01 +0900

    refactor: 예외 응답 doc 를 만들기 위해 예외 핸들러를 test 패키지로 이동

--- src/main/java/revi1337/onsquad/common/error/RestErrorHandler.java
+++ src/main/java/revi1337/onsquad/common/error/RestErrorHandler.java
@@ -1,11 +1,7 @@
 package revi1337.onsquad.common.error;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
-import org.springframework.core.type.filter.AssignableTypeFilter;
-import org.springframework.core.type.filter.TypeFilter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import revi1337.onsquad.common.dto.CommonErrorCode;
 import revi1337.onsquad.common.dto.ProblemDetail;
 import revi1337.onsquad.common.dto.RestResponse;
-
-import java.util.*;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -34,37 +28,5 @@ public class RestErrorHandler implements ErrorController {
             default -> ResponseEntity.status(INTERNAL_SERVER_ERROR)
                     .body(RestResponse.fail(ProblemDetail.of(CommonErrorCode.INTERNAL_SERVER_ERROR)));
         };
-    }
-
-    @RequestMapping("/errors")
-    public RestResponse<Map<String, String>> handleErrors() {
-        ClassPathScanningCandidateComponentProvider classPathScanningCandidateComponentProvider =
-                new ClassPathScanningCandidateComponentProvider(false);
-        TypeFilter typeFilter = new AssignableTypeFilter(ErrorCode.class);
-        classPathScanningCandidateComponentProvider.addIncludeFilter(typeFilter);
-
-        return RestResponse.success(
-                buildCommonErrorResponse(classPathScanningCandidateComponentProvider)
-        );
-    }
-
-    private Map<String, String> buildCommonErrorResponse(ClassPathScanningCandidateComponentProvider provider) {
-        Set<BeanDefinition> components = provider.findCandidateComponents("revi1337.onsquad");
-        Map<String, String> errors = new HashMap<>();
-        for (BeanDefinition component : components) {
-            try {
-                Class<?> className = Class.forName(component.getBeanClassName());
-                if (className.isEnum()) {
-                    for (var errorCode : className.getEnumConstants()) {
-                        String code = (String) errorCode.getClass().getMethod("getCode").invoke(errorCode);
-                        String description = (String) errorCode.getClass().getMethod("getDescription").invoke(errorCode);
-                        errors.put(code, description);
-                    }
-                }
-            } catch (Exception e) {
-                throw new RuntimeException("unexpected error occurred");
-            }
-        }
-        return errors;
     }
 }
---
+++ src/test/java/revi1337/onsquad/common/error/RestErrorHandlerTest.java
@@ -0,0 +1,76 @@
+package revi1337.onsquad.common.error;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AssignableTypeFilter;
+import org.springframework.core.type.filter.TypeFilter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import revi1337.onsquad.common.dto.RestResponse;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RestController
+class RestErrorHandlerTest {
+
+    private static final String BASE_PACKAGE = "revi1337.onsquad";
+
+    @GetMapping("/errors")
+    public RestResponse<Map<String, String>> handleErrors() {
+        ClassPathScanningCandidateComponentProvider classPathScanningCandidateComponentProvider =
+                new ClassPathScanningCandidateComponentProvider(false);
+        TypeFilter typeFilter = new AssignableTypeFilter(ErrorCode.class);
+        classPathScanningCandidateComponentProvider.addIncludeFilter(typeFilter);
+
+        return RestResponse.success(
+                buildCommonErrorResponse(classPathScanningCandidateComponentProvider)
+        );
+    }
+
+    private Map<String, String> buildCommonErrorResponse(ClassPathScanningCandidateComponentProvider provider) {
+        Set<BeanDefinition> components = provider.findCandidateComponents(BASE_PACKAGE);
+        List<ErrorCode> errorCodes = new ArrayList<>();
+        for (BeanDefinition component : components) {
+            try {
+                Class<?> className = Class.forName(component.getBeanClassName());
+                if (className.isEnum()) {
+                    errorCodes.addAll(List.of((ErrorCode[]) className.getEnumConstants()));
+                }
+            } catch (Exception e) {
+                throw new RuntimeException("unexpected error occurred");
+            }
+        }
+
+        return errorCodes.stream().collect(
+                Collectors.toMap(ErrorCode::getCode, ErrorCode::getDescription)
+        );
+    }
+}
+
+//    private Map<String, String> buildCommonErrorResponse(ClassPathScanningCandidateComponentProvider provider) {
+//        Set<BeanDefinition> components = provider.findCandidateComponents(BASE_PACKAGE);
+//        Map<String, String> errors = new HashMap<>();
+//        for (BeanDefinition component : components) {
+//            try {
+//                Class<?> className = Class.forName(component.getBeanClassName());
+//                if (className.isEnum()) {
+//                    Object[] constants = className.getEnumConstants();
+//                    for (var errorCode : constants) {
+////                        values = (ErrorCode[]) errorCode.getClass().getMethod("values").invoke(errorCode);
+//                        String code = (String) errorCode.getClass().getMethod("getCode").invoke(errorCode);
+//                        String description = (String) errorCode.getClass().getMethod("getDescription").invoke(errorCode);
+//                        errors.put(code, description);
+//                    }
+//                    ErrorCode[] enumConstants = (ErrorCode[]) constants;
+//                    for (ErrorCode enumConstant : enumConstants) {
+//                        System.out.println("enumConstant = " + enumConstant.getDescription());
+//                    }
+//                    System.out.println();
+//                }
+//            } catch (Exception e) {
+//                throw new RuntimeException("unexpected error occurred");
+//            }
+//        }
+//        return errors;
+//    }
