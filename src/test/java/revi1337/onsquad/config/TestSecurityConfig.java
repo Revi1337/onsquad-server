2e81f96184000520f350ea7c61e3c1efa2a4d71f
Revi1337 <david122123@gmail.com>
2024-04-16 18:13:42 +0900

    test: 테스트 전용 security config 작성

---
+++ src/test/java/revi1337/onsquad/config/TestSecurityConfig.java
@@ -0,0 +1,21 @@
+package revi1337.onsquad.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@TestConfiguration
+public class TestSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll())
+                .build();
+    }
+}
---
+++ src/test/java/revi1337/onsquad/support/ValidationTestSupport.java
@@ -0,0 +1,31 @@
+package revi1337.onsquad.support;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import revi1337.onsquad.config.SpringActiveProfilesResolver;
+import revi1337.onsquad.config.TestSecurityConfig;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@WebMvcTest
+@Import(TestSecurityConfig.class)
+@ActiveProfiles(resolver = SpringActiveProfilesResolver.class)
+public abstract class ValidationTestSupport {
+
+    protected MockMvc mockMvc;
+    @Autowired protected ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp(final WebApplicationContext applicationContext) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext)
+                .alwaysDo(print())
+                .build();
+    }
+}
