55428423c3203a21020e22163c211d10827ec248
Revi1337 <david122123@gmail.com>
2024-04-27 00:48:08 +0900

    chore: 초기 사용자의 패스워드 해싱

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into member(email, address, nickname, password)
+values ('david122123@gmail.com1', '어딘가1', '닉네임1', '{bcrypt}$2a$10$RIWcAfMxZ1E9BsL60OcMVOiEsSumcCDqx0snDp6q8lNuxQI2XcSbi'),
+       ('david122123@gmail.com2', '어딘가2', '닉네임2', '{bcrypt}$2a$10$RIWcAfMxZ1E9BsL60OcMVOiEsSumcCDqx0snDp6q8lNuxQI2XcSbi'),
+       ('david122123@gmail.com3', '어딘가3', '닉네임3', '{bcrypt}$2a$10$RIWcAfMxZ1E9BsL60OcMVOiEsSumcCDqx0snDp6q8lNuxQI2XcSbi');
