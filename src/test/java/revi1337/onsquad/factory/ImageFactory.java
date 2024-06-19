47217afa14ea43318495b7bae061b108bf5a74b5
Revi1337 <david122123@gmail.com>
2024-06-19 21:56:11 +0900

    test: crew 정보 수정 비즈니스 로직 구현 테스트

--- src/test/java/revi1337/onsquad/factory/ImageFactory.java
+++ src/test/java/revi1337/onsquad/factory/ImageFactory.java
@@ -4,9 +4,10 @@ import revi1337.onsquad.image.domain.Image;
 
 public class ImageFactory {
 
-    public static final byte[] IMAGE_DATA = {(byte) 0xFF, (byte) 0xD8, (byte) 0xFF};
+    public static final byte[] JPG_IMAGE = {(byte) 0xFF, (byte) 0xD8, (byte) 0xFF}; // JPG, JPEG
+    public static final byte[] PNG_IMAGE = {(byte) 0x89, (byte) 0x50, (byte) 0x4E, (byte) 0x47, (byte) 0x0D, (byte) 0x0A, (byte) 0x1A, (byte) 0x0A}; // PNG
 
     public static Image defaultImage() {
-        return new Image();
+        return new Image(JPG_IMAGE);
     }
 }
