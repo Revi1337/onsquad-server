885d3033501a9b92edf21b64e2a961c0b483b2e7
Revi1337 <david122123@gmail.com>
2024-06-26 01:40:19 +0900

    feat: comment 도메인 로직 작성

---
+++ src/main/java/revi1337/onsquad/comment/domain/Comment.java
@@ -0,0 +1,79 @@
+package revi1337.onsquad.comment.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import revi1337.onsquad.comment.error.exception.CommentDomainException;
+import revi1337.onsquad.common.domain.BaseEntity;
+import revi1337.onsquad.crew.domain.Crew;
+import revi1337.onsquad.member.domain.Member;
+
+import java.util.Objects;
+
+import static revi1337.onsquad.comment.error.CommentErrorCode.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Comment extends BaseEntity {
+
+    private static final int MAX_LENGTH = 250;
+    private static final int PERSIST_MAX_LENGTH = MAX_LENGTH * 3;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = PERSIST_MAX_LENGTH)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "crew_id", nullable = false)
+    private Crew crew;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    public Comment(String content) {
+        validate(content);
+        this.content = content;
+    }
+
+    private void validate(String content) {
+        if (content == null) {
+            throw new NullPointerException("댓글은 null 일 수 없습니다.");
+        }
+
+        if (content.isEmpty() || content.length() > MAX_LENGTH) {
+            throw new CommentDomainException.InvalidLength(INVALID_LENGTH, MAX_LENGTH);
+        }
+    }
+
+    @Builder
+    private Comment(Long id, String content, Crew crew, Member member) {
+        this.id = id;
+        this.content = content;
+        this.crew = crew;
+        this.member = member;
+    }
+
+    public static Comment of(String content, Crew crew, Member member) {
+        return Comment.builder()
+                .content(content)
+                .crew(crew)
+                .member(member)
+                .build();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Comment comment)) return false;
+        return id != null && Objects.equals(getId(), comment.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+}
---
+++ src/main/java/revi1337/onsquad/comment/domain/CommentRepository.java
@@ -0,0 +1,7 @@
+package revi1337.onsquad.comment.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
+
