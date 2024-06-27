01038839d2d18aa4cad6ceb6d7c95850679e13b4
Revi1337 <david122123@gmail.com>
2024-06-27 19:08:46 +0900

    feat: comment 비지니스 로직에서 일어날 수 있는 exception 과 handler 작성

--- src/main/java/revi1337/onsquad/comment/error/handler/CommentExceptionHandler.java
+++ src/main/java/revi1337/onsquad/comment/error/handler/CommentExceptionHandler.java
@@ -3,6 +3,7 @@ package revi1337.onsquad.comment.error.handler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import revi1337.onsquad.comment.error.exception.CommentBusinessException;
 import revi1337.onsquad.comment.error.exception.CommentDomainException;
 import revi1337.onsquad.common.dto.ProblemDetail;
 import revi1337.onsquad.common.dto.RestResponse;
@@ -14,6 +15,16 @@ public class CommentExceptionHandler {
     @ExceptionHandler(CommentDomainException.class)
     public ResponseEntity<RestResponse<ProblemDetail>> handleCommentDomainException(
             CommentDomainException exception
+    ) {
+        ErrorCode errorCode = exception.getErrorCode();
+        ProblemDetail problemDetail = ProblemDetail.of(errorCode, exception.getErrorMessage());
+        RestResponse<ProblemDetail> restResponse = RestResponse.fail(problemDetail);
+        return ResponseEntity.status(errorCode.getStatus()).body(restResponse);
+    }
+
+    @ExceptionHandler(CommentBusinessException.class)
+    public ResponseEntity<RestResponse<ProblemDetail>> handleCommentBusinessException(
+            CommentBusinessException exception
     ) {
         ErrorCode errorCode = exception.getErrorCode();
         ProblemDetail problemDetail = ProblemDetail.of(errorCode, exception.getErrorMessage());
