830638171cdf0c9d86518d746508be84c9366723
Revi1337 <david122123@gmail.com>
2024-06-29 20:26:28 +0900

    test: 쿼리 튜닝 및 테이블 파티션 기능 테스트코드 작성

--- src/test/java/revi1337/onsquad/comment/domain/CommentRepositoryTest.java
+++ src/test/java/revi1337/onsquad/comment/domain/CommentRepositoryTest.java
@@ -1,5 +1,6 @@
 package revi1337.onsquad.comment.domain;
 
+import com.querydsl.core.Tuple;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -301,7 +302,7 @@ class CommentRepositoryTest extends PersistenceLayerTestSupport {
             System.out.println("comment = " + comment);
         }
     }
-    
+
     @Test
     @DisplayName("부모 댓글들 조회 후 --> 그 id 들로 자식댓글들을 찾아오는 방식 not transform")
     public void transformTest() {
@@ -445,7 +446,7 @@ class CommentRepositoryTest extends PersistenceLayerTestSupport {
                 .map(Comment::getId)
                 .toList();
 
-        Map<Comment, List<Comment>> groupedByParents = commentRepository.findGroupedChildCommentsByParentIdIn(parentIds);
+        Map<Comment, List<Comment>> groupedByParents = commentRepository.findGroupedChildCommentsByParentIdIn(parentIds, null);
 
         // 최상위 부모 엔티티를 CommentsDto로 변환
         Map<Long, CommentsDto> commentDtoMap = parentComments.stream()
@@ -466,5 +467,77 @@ class CommentRepositoryTest extends PersistenceLayerTestSupport {
                 .toList();
         LocalDateTime end = LocalDateTime.now();
         System.out.println(Duration.between(st, end).toNanos());
+    }
+
+    @Test
+    @DisplayName("부모 댓글들 조회 후 --> 그 id 들로 자식댓글들을 찾아오는 방식 transform")
+    public void transformTest3() {
+        Member member1 = MemberFactory.defaultMember().build();
+        Member member2 = MemberFactory.defaultMember().build();
+        Member member3 = MemberFactory.defaultMember().build();
+        Image image1 = ImageFactory.defaultImage();
+        Image image2 = ImageFactory.defaultImage();
+        Crew crew1 = CrewFactory.defaultCrew().name(new Name("크루 이름 1")).image(image1).member(member1).build();
+        Crew crew2 = CrewFactory.defaultCrew().name(new Name("크루 이름 2")).image(image2).member(member2).build();
+        Comment comment1 = Comment.of("댓글 4", crew1, member1);
+        Comment comment2 = Comment.of("댓글 3", crew1, member1);
+        Comment comment3 = Comment.of("댓글 2", crew1, member1);
+        Comment comment4 = Comment.of("댓글 1", crew1, member1);
+        memberRepository.saveAll(List.of(member1, member2, member3));
+        crewRepository.saveAll(List.of(crew1, crew2));
+        commentRepository.saveAll(List.of(comment1, comment2, comment3, comment4));
+        Comment reply1 = Comment.forReply(comment1, "대댓글 1", crew1, member1);
+        Comment reply2 = Comment.forReply(comment1, "대댓글 2", crew1, member2);
+        Comment reply3 = Comment.forReply(comment1, "대댓글 3", crew1, member2);
+        Comment reply4 = Comment.forReply(comment2, "대댓글 4", crew1, member2);
+        Comment reply5 = Comment.forReply(comment2, "대댓글 5", crew1, member1);
+        Comment reply6 = Comment.forReply(comment2, "대댓글 6", crew1, member1);
+        Comment reply7 = Comment.forReply(comment3, "대댓글 7", crew1, member2);
+        Comment reply8 = Comment.forReply(comment3, "대댓글 8", crew1, member1);
+        Comment reply9 = Comment.forReply(comment3, "대댓글 9", crew1, member1);
+        Comment reply10 = Comment.forReply(comment3, "대댓글 9", crew1, member1);
+        Comment reply11 = Comment.forReply(comment3, "대댓글 9", crew1, member1);
+        Comment reply12 = Comment.forReply(comment3, "대댓글 9", crew1, member1);
+        Comment reply13 = Comment.forReply(comment3, "대댓글 9", crew1, member1);
+        Comment reply14 = Comment.forReply(comment3, "대댓글 9", crew1, member1);
+        Comment reply15 = Comment.forReply(comment3, "대댓글 9", crew2, member1);
+        Comment reply16 = Comment.forReply(comment3, "대댓글 9", crew2, member1);
+        Comment reply17 = Comment.forReply(comment3, "대댓글 9", crew2, member1);
+        Comment reply18 = Comment.forReply(comment3, "대댓글 9", crew2, member1);
+        Comment reply19 = Comment.forReply(comment3, "대댓글 9", crew2, member1);
+        Comment reply20 = Comment.forReply(comment3, "대댓글 9", crew2, member1);
+        Comment reply21 = Comment.forReply(comment3, "대댓글 9", crew2, member1);
+        Comment reply22 = Comment.forReply(comment3, "대댓글 9", crew2, member1);
+        Comment reply23 = Comment.forReply(comment3, "대댓글 9", crew2, member1);
+        Comment reply24 = Comment.forReply(comment3, "대댓글 9", crew2, member1);
+        Comment reply25 = Comment.forReply(comment3, "대댓글 9", crew2, member1);
+        Comment reply26 = Comment.forReply(comment3, "대댓글 9", crew2, member1);
+
+        commentRepository.saveAll(
+                List.of(
+                        reply1, reply2, reply3, reply4, reply5, reply6, reply7, reply8, reply9, reply10,
+                        reply11, reply12, reply13, reply14, reply15, reply16, reply17, reply18, reply19,
+                        reply20, reply21, reply22, reply23, reply24, reply25, reply26
+                )
+        );
+
+        entityManager.flush();
+        entityManager.clear();
+
+        LocalDateTime st = LocalDateTime.now();
+        List<Comment> parentComments = commentRepository.findParentCommentsByCrewName(new Name("크루 이름 1"));
+        List<Long> parentIds = parentComments.stream()
+                .map(Comment::getId)
+                .toList();
+
+        List<Tuple> groupedChildCommentsByParentIdIn2 = commentRepository.findGroupedChildCommentsByParentIdIn2(parentIds);
+//        for (Tuple tuple : groupedChildCommentsByParentIdIn2) {
+//            Comment comment = tuple.get(0, Comment.class);
+//            Long number = tuple.get(1, Long.class);
+//            if (comment.getParent().getId() == 3L) {
+//                System.out.println("parent Id --> " + comment.getParent().getId());
+//                System.out.println(number);
+//            }
+//        }
     }
 }
--- src/test/java/revi1337/onsquad/config/TestQueryDslConfig.java
+++ src/test/java/revi1337/onsquad/config/TestQueryDslConfig.java
@@ -1,7 +1,10 @@
 package revi1337.onsquad.config;
 
+import com.blazebit.persistence.querydsl.JPQLNextOps;
+import com.querydsl.jpa.Hibernate5Templates;
 import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.sql.SQLOps;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -15,6 +18,25 @@ public class TestQueryDslConfig {
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
-        return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+//        return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+        return new JPAQueryFactory(new CustomJPQLTemplates(), entityManager);
+    }
+
+    static class CustomJPQLTemplates extends Hibernate5Templates {
+
+        public CustomJPQLTemplates() {
+            add(SQLOps.ROWNUMBER, "row_number()");
+        }
+
+//        public CustomJPQLTemplates() {
+//            add(SQLOps.ROWNUMBER, "row_number()");
+//            add(JPQLNextOps.ROW_NUMBER, "row_number()");
+//            add(JPQLNextOps.WINDOW_ORDER_BY, "ORDER BY {0}");
+//            add(JPQLNextOps.WINDOW_PARTITION_BY, "PARTITION BY {0}");
+//            add(JPQLNextOps.WINDOW_DEFINITION_1, "{0}");
+//            add(JPQLNextOps.WINDOW_DEFINITION_2, "{0} {1}");
+//            add(JPQLNextOps.WINDOW_DEFINITION_3, "{0} {1} {2}");
+//            add(JPQLNextOps.WINDOW_DEFINITION_4, "{0} {1} {2} {3}");
+//        }
     }
 }
