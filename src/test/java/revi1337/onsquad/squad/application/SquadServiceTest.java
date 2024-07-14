ad6d9e2d9d4325fbff9e6e98b2d6ba5f90fda525
Revi1337 <david122123@gmail.com>
2024-07-15 04:03:15 +0900

    test: squad 참가 신청 application layer 테스트코드 작성

--- src/test/java/revi1337/onsquad/squad/application/SquadServiceTest.java
+++ src/test/java/revi1337/onsquad/squad/application/SquadServiceTest.java
@@ -11,16 +11,22 @@ import revi1337.onsquad.crew.domain.CrewRepository;
 import revi1337.onsquad.crew.domain.vo.Name;
 import revi1337.onsquad.crew.error.exception.CrewBusinessException;
 import revi1337.onsquad.crew_member.domain.CrewMember;
+import revi1337.onsquad.crew_member.domain.CrewMemberRepository;
 import revi1337.onsquad.crew_member.domain.vo.JoinStatus;
 import revi1337.onsquad.crew_member.error.exception.CrewMemberBusinessException;
 import revi1337.onsquad.factory.CrewFactory;
 import revi1337.onsquad.factory.MemberFactory;
+import revi1337.onsquad.factory.SquadFactory;
 import revi1337.onsquad.member.domain.Member;
 import revi1337.onsquad.member.domain.MemberRepository;
 import revi1337.onsquad.member.error.exception.MemberBusinessException;
 import revi1337.onsquad.squad.domain.Squad;
 import revi1337.onsquad.squad.domain.SquadRepository;
+import revi1337.onsquad.squad.domain.vo.Title;
 import revi1337.onsquad.squad.dto.SquadCreateDto;
+import revi1337.onsquad.squad.dto.SquadJoinDto;
+import revi1337.onsquad.squad.error.exception.SquadBusinessException;
+import revi1337.onsquad.squad_member.domain.SquadMember;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,6 +41,7 @@ class SquadServiceTest {
     @Mock private MemberRepository memberRepository;
     @Mock private SquadRepository squadRepository;
     @Mock private CrewRepository crewRepository;
+    @Mock private CrewMemberRepository crewMemberRepository;
     @InjectMocks private SquadService squadService;
 
     @Test
@@ -121,6 +128,119 @@ class SquadServiceTest {
 
         // when & then
         assertThatThrownBy(() -> squadService.createNewSquad(squadCreateDto, memberId))
+                .isInstanceOf(CrewMemberBusinessException.NotParticipant.class)
+                .hasMessage("id 가 1 인 사용자는 크루 1 크루에 속해있지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("Squad 참여 요청에 성공한다.")
+    public void joinSquad1() {
+        // given
+        SquadJoinDto squadJoinDto = new SquadJoinDto("크루 1", 1L, "스쿼드 제목");
+        Long memberId = 1L;
+        Member member = MemberFactory.defaultMember().id(memberId).build();
+        Crew crew = CrewFactory.defaultCrew().member(member).build();
+        Squad squad = SquadFactory.defaultSquad().crew(crew).member(member).build();
+        given(memberRepository.findById(memberId)).willReturn(Optional.of(member));
+        given(crewMemberRepository.existsCrewMember(memberId)).willReturn(true);
+        given(squadRepository.findSquadWithMembersById(squadJoinDto.squadId(), new Title(squadJoinDto.squadTitle()))).willReturn(Optional.of(squad));
+
+        // when
+        squadService.joinSquad(squadJoinDto, memberId);
+
+        // then
+        then(squadRepository).should(times(1)).saveAndFlush(squad);
+    }
+
+    @Test
+    @DisplayName("이미 Squad 에 참여 요청을 한적있으면 실패한다.")
+    public void joinSquad2() {
+        // given
+        SquadJoinDto squadJoinDto = new SquadJoinDto("크루 1", 1L, "스쿼드 제목");
+        Long memberId = 1L;
+        Member member = MemberFactory.defaultMember().id(memberId).build();
+        Crew crew = CrewFactory.defaultCrew().member(member).build();
+        Squad squad = SquadFactory.defaultSquad().crew(crew).member(member).build();
+        SquadMember squadMember = SquadMember.forGeneral(member);
+        squad.addSquadMember(squadMember);
+        given(memberRepository.findById(memberId)).willReturn(Optional.of(member));
+        given(crewMemberRepository.existsCrewMember(memberId)).willReturn(true);
+        given(squadRepository.findSquadWithMembersById(squadJoinDto.squadId(), new Title(squadJoinDto.squadTitle()))).willReturn(Optional.of(squad));
+
+        // when & then
+        assertThatThrownBy(() -> squadService.joinSquad(squadJoinDto, memberId))
+                .isInstanceOf(SquadBusinessException.AlreadyRequest.class)
+                .hasMessage("스쿼드 제목 스쿼드에 참여요청을 한 이력이 있습니다.");
+    }
+
+    @Test
+    @DisplayName("이미 Squad 에 이미 속한 사용자면 실패한다.")
+    public void joinSquad3() {
+        // given
+        SquadJoinDto squadJoinDto = new SquadJoinDto("크루 1", 1L, "스쿼드 제목");
+        Long memberId = 1L;
+        Member member = MemberFactory.defaultMember().id(memberId).build();
+        Crew crew = CrewFactory.defaultCrew().member(member).build();
+        Squad squad = SquadFactory.defaultSquad().crew(crew).member(member).build();
+        SquadMember squadMember = SquadMember.forLeader(member);
+        squad.addSquadMember(squadMember);
+        given(memberRepository.findById(memberId)).willReturn(Optional.of(member));
+        given(crewMemberRepository.existsCrewMember(memberId)).willReturn(true);
+        given(squadRepository.findSquadWithMembersById(squadJoinDto.squadId(), new Title(squadJoinDto.squadTitle()))).willReturn(Optional.of(squad));
+
+        // when & then
+        assertThatThrownBy(() -> squadService.joinSquad(squadJoinDto, memberId))
+                .isInstanceOf(SquadBusinessException.AlreadyParticipant.class)
+                .hasMessage("이미 스쿼드 제목 스쿼드에 가입된 사용자입니다.");
+    }
+
+    @Test
+    @DisplayName("Squad 가 없으면 Squad 에 참여신청할 수 없다.")
+    public void joinSquad4() {
+        // given
+        SquadJoinDto squadJoinDto = new SquadJoinDto("크루 1", 1L, "스쿼드 제목");
+        Long memberId = 1L;
+        Member member = MemberFactory.defaultMember().id(memberId).build();
+        Crew crew = CrewFactory.defaultCrew().member(member).build();
+        Squad squad = SquadFactory.defaultSquad().crew(crew).member(member).build();
+        SquadMember squadMember = SquadMember.forLeader(member);
+        squad.addSquadMember(squadMember);
+        given(memberRepository.findById(memberId)).willReturn(Optional.of(member));
+        given(crewMemberRepository.existsCrewMember(memberId)).willReturn(true);
+        given(squadRepository.findSquadWithMembersById(squadJoinDto.squadId(), new Title(squadJoinDto.squadTitle()))).willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> squadService.joinSquad(squadJoinDto, memberId))
+                .isInstanceOf(SquadBusinessException.NotFound.class)
+                .hasMessage("스쿼드 제목 스쿼드를 찾을 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("Member 가 없으면 Squad 에 참여신청할 수 없다.")
+    public void joinSquad5() {
+        // given
+        SquadJoinDto squadJoinDto = new SquadJoinDto("크루 1", 1L, "스쿼드 제목");
+        Long memberId = 1L;
+        given(memberRepository.findById(memberId)).willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> squadService.joinSquad(squadJoinDto, memberId))
+                .isInstanceOf(MemberBusinessException.NotFound.class)
+                .hasMessage("id 가 1 인 사용자를 찾을 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("Member 아 Crew 에 속해있지 않으면 Squad 에 참여신청할 수 없다.")
+    public void joinSquad6() {
+        // given
+        SquadJoinDto squadJoinDto = new SquadJoinDto("크루 1", 1L, "스쿼드 제목");
+        Long memberId = 1L;
+        Member member = MemberFactory.defaultMember().id(memberId).build();
+        given(memberRepository.findById(memberId)).willReturn(Optional.of(member));
+        given(crewMemberRepository.existsCrewMember(memberId)).willReturn(false);
+
+        // when & then
+        assertThatThrownBy(() -> squadService.joinSquad(squadJoinDto, memberId))
                 .isInstanceOf(CrewMemberBusinessException.NotParticipant.class)
                 .hasMessage("id 가 1 인 사용자는 크루 1 크루에 속해있지 않습니다.");
     }
