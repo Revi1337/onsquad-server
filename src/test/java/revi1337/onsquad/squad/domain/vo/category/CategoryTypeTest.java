c1fea2d06897c060de040ce26cf8d275fe51d6a3
Revi1337 <david122123@gmail.com>
2024-08-26 15:17:14 +0900

    test: 카테고리 도메인 테스트 코드 작성

---
+++ src/test/java/revi1337/onsquad/squad/domain/vo/category/CategoryTypeTest.java
@@ -0,0 +1,48 @@
+package revi1337.onsquad.squad.domain.vo.category;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+@DisplayName("CategoryType 테스트")
+class CategoryTypeTest {
+
+    @Test
+    @DisplayName("문자열을 통해 CategoryType 들을 생성한다.")
+    public void categoryTest1() {
+        // given
+        List<String> stringCategories = List.of("전체", "등산");
+
+        // when
+        List<CategoryType> categoryTypes = CategoryType.fromTexts(stringCategories);
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(categoryTypes).hasSize(2);
+            softly.assertThat(categoryTypes).element(0).isEqualTo(CategoryType.ALL);
+            softly.assertThat(categoryTypes).element(1).isEqualTo(CategoryType.HIKING);
+        });
+    }
+
+    @Test
+    @DisplayName("문자열을 통해 CategoryType 들을 생성한다. 2")
+    public void categoryTest2() {
+        // given
+        String dummyCategoryText = "dummy_category";
+        List<String> stringCategories = List.of("전체", "등산", dummyCategoryText);
+
+        // when
+        List<CategoryType> categoryTypes = CategoryType.fromTexts(stringCategories);
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(categoryTypes).hasSize(3);
+            softly.assertThat(categoryTypes).element(0).isEqualTo(CategoryType.ALL);
+            softly.assertThat(categoryTypes).element(1).isEqualTo(CategoryType.HIKING);
+            softly.assertThat(categoryTypes).element(2).isNull();
+        });
+    }
+}
