12bb50edad7e98f10ba3114cac1b48573b34ad4b
Revi1337 <david122123@gmail.com>
2024-08-26 11:55:56 +0900

    feat: squad-category 도메인 코드 작성
    - n:m 관계를 1:n, n:1 관계로 단순화하였기 때문에 Squad 와 Cagtegory 사이에 SquadCategory 테이블을 두어 복잡성을 낮춘다.

---
+++ src/main/java/revi1337/onsquad/squad/domain/squad_category/SquadCategory.java
@@ -0,0 +1,27 @@
+package revi1337.onsquad.squad.domain.squad_category;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import revi1337.onsquad.squad.domain.Squad;
+import revi1337.onsquad.squad.domain.category.Category;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class SquadCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "squad_id", nullable = false)
+    private Squad squad;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+}
---
+++ src/main/java/revi1337/onsquad/squad/domain/squad_category/SquadCategoryJdbcRepository.java
@@ -0,0 +1,28 @@
+package revi1337.onsquad.squad.domain.squad_category;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import revi1337.onsquad.squad.domain.category.Category;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class SquadCategoryJdbcRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public void batchInsertSquadCategories(Long squadId, List<Category> categories) {
+        String sql = "INSERT INTO squad_category(squad_id, category_id) VALUES (?, ?)";
+        jdbcTemplate.batchUpdate(
+                sql,
+                categories,
+                categories.size(),
+                (ps, category) -> {
+                    ps.setLong(1, squadId);
+                    ps.setLong(2, category.getId());
+                }
+        );
+    }
+}
