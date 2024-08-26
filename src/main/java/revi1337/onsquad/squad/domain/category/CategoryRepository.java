355b6e874c8bdf708620d8ee5fd374cf3010ad37
Revi1337 <david122123@gmail.com>
2024-08-26 11:52:25 +0900

    feat: 카테고리 1차 정규화 및 second-cache 설정

---
+++ src/main/java/revi1337/onsquad/squad/domain/category/CategoryRepository.java
@@ -0,0 +1,20 @@
+package revi1337.onsquad.squad.domain.category;
+
+import revi1337.onsquad.squad.domain.category.vo.CategoryType;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepository {
+
+    void insertBulkCategories(List<Category> categories);
+
+    void saveAll(List<Category> categories);
+
+    List<Category> findAll();
+
+    Optional<Category> findById(Long id);
+
+    List<Category> findCategoriesInSecondCache(List<CategoryType> categoryTypes);
+
+}
