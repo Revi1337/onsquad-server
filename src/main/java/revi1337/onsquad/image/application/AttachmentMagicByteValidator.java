e1f5bdc52096f0ae212ab289923e1e3d6a118ff8
Revi1337 <david122123@gmail.com>
2024-10-03 15:03:06 +0900

    feat: webp 형태의 magic number 도 업로드할 수 있게 허용합니다

--- src/main/java/revi1337/onsquad/image/application/AttachmentMagicByteValidator.java
+++ src/main/java/revi1337/onsquad/image/application/AttachmentMagicByteValidator.java
@@ -3,19 +3,13 @@ package revi1337.onsquad.image.application;
 import revi1337.onsquad.image.domain.vo.SupportAttachmentType;
 import revi1337.onsquad.image.error.exception.AttachmentValidationException;
 
-import java.util.Arrays;
-
-import static revi1337.onsquad.image.error.AttachmentErrorCode.*;
+import static revi1337.onsquad.image.error.AttachmentErrorCode.UNSUPPORTED_MAGIC_BYTE;
 
 public abstract class AttachmentMagicByteValidator {
 
     public static void validateMagicByte(byte[] binary) {
         boolean isValid = SupportAttachmentType.defaultEnumSet().stream()
-                .anyMatch(support -> {
-                    byte[] supportMagicByte = support.getMagicByte();
-                    byte[] binaryMagicByte = Arrays.copyOfRange(binary, 0, supportMagicByte.length);
-                    return Arrays.equals(supportMagicByte, binaryMagicByte);
-                });
+                .anyMatch(support -> support.matches(binary));
 
         if (!isValid) {
             throw new AttachmentValidationException.UnsupportedAttachmentType(
--- src/main/java/revi1337/onsquad/image/domain/vo/SupportAttachmentType.java
+++ src/main/java/revi1337/onsquad/image/domain/vo/SupportAttachmentType.java
@@ -2,25 +2,70 @@ package revi1337.onsquad.image.domain.vo;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public enum SupportAttachmentType {
 
-    JPEG(new byte[]{(byte) 0xFF, (byte) 0xD8, (byte) 0xFF}),
-    JPG(new byte[]{(byte) 0xFF, (byte) 0xD8, (byte) 0xFF}),
-    PNG(new byte[]{(byte) 0x89, (byte) 0x50, (byte) 0x4E, (byte) 0x47, (byte) 0x0D, (byte) 0x0A, (byte) 0x1A, (byte) 0x0A}),
-    SVG(new byte[]{(byte) 0x3C, (byte) 0x3F, (byte) 0x78, (byte) 0x6D, (byte) 0x6C, (byte) 0x20, (byte) 0x76, (byte) 0x65, (byte) 0x72, (byte) 0x73, (byte) 0x69, (byte) 0x6F, (byte) 0x6E, (byte) 0x3D});
+    JPEG(
+            new byte[]{(byte) 0xFF, (byte) 0xD8, (byte) 0xFF},
+            new int[][]{}
+    ),
+    JPG(
+            new byte[]{(byte) 0xFF, (byte) 0xD8, (byte) 0xFF},
+            new int[][]{}
+    ),
+    PNG(
+            new byte[]{(byte) 0x89, (byte) 0x50, (byte) 0x4E, (byte) 0x47, (byte) 0x0D, (byte) 0x0A, (byte) 0x1A, (byte) 0x0A},
+            new int[][]{}
+    ),
+    SVG(
+            new byte[]{(byte) 0x3C, (byte) 0x3F, (byte) 0x78, (byte) 0x6D, (byte) 0x6C, (byte) 0x20, (byte) 0x76, (byte) 0x65, (byte) 0x72, (byte) 0x73, (byte) 0x69, (byte) 0x6F, (byte) 0x6E, (byte) 0x3D},
+            new int[][]{}
+    ),
+    WEBP(
+            new byte[]{(byte) 0x52, (byte) 0x49, (byte) 0x46, (byte) 0x46, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x57, (byte) 0x45, (byte) 0x42, (byte) 0x50},
+            new int[][]{{4, 8}}
+    )
+    ;
 
     private final byte[] magicByte;
+    private final int[][] partialOffsets;
 
     public byte[] getMagicByte() {
         return magicByte.clone();
     }
 
+    public int[][] partialOffsets() {
+        return partialOffsets.clone();
+    }
+
     public static EnumSet<SupportAttachmentType> defaultEnumSet() {
         return EnumSet.allOf(SupportAttachmentType.class);
+    }
+
+    public boolean matches(byte[] binary) {
+        byte[] magicByte = getMagicByte();
+        int[][] partialOffsets = partialOffsets();
+
+        if (partialOffsets.length == 0) {
+            return Arrays.equals(Arrays.copyOfRange(binary, 0, magicByte.length), magicByte);
+        }
+        for (int[] partialOffset : partialOffsets) {
+            int start = partialOffset[0];
+            int end = partialOffset[1];
+            for (int i = 0; i < magicByte.length; i++) {
+                if (i < start || i >= end) {
+                    if (binary.length <= i || binary[i] != magicByte[i]) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
     }
 
     public static String convertSupportedTypeString() {
