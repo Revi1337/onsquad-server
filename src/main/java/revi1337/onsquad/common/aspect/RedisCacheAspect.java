bad1b79a0a9fc4364eacd80562e0756e7dba6412
Revi1337 <david122123@gmail.com>
2025-03-07 21:43:44 +0900

    refactor: AOP 를 이용한 RedisCache 로직을 리팩토링합니다

--- src/main/java/revi1337/onsquad/common/aspect/RedisCacheAspect.java
+++ src/main/java/revi1337/onsquad/common/aspect/RedisCacheAspect.java
@@ -8,7 +8,6 @@ import java.lang.reflect.Type;
 import java.time.Duration;
 import java.util.Collection;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
@@ -29,109 +28,104 @@ public class RedisCacheAspect {
 
     @Around("@annotation(redisCache)")
     public Object handleRedisCache(ProceedingJoinPoint joinPoint, RedisCache redisCache) {
+        String redisKey = generateRedisKey(joinPoint, redisCache);
+        Object cachedData = redisTemplate.opsForValue().get(redisKey);
+        if (cachedData != null) {
+            return deserializeCachedData(joinPoint, cachedData);
+        }
         try {
-            OnSquadType onsquadType = redisCache.type();
-            MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-            String redisKey = getRedisKey(joinPoint, redisCache, methodSignature, onsquadType);
-            Object cachedData = redisTemplate.opsForValue().get(redisKey);
-
-            Method method = methodSignature.getMethod();
-            ParameterizedType genericReturnType = resolveGenericReturnType(method);
-            if (cachedData != null && genericReturnType == null) {
-                return objectMapper.convertValue(cachedData, method.getReturnType());
+            Object result = joinPoint.proceed();
+            if (shouldCache(redisCache, result)) {
+                redisTemplate.opsForValue().set(redisKey, result, calculateTtl(redisCache));
             }
-
-            if (cachedData != null) {
-                Class<?> actualGenericClass = retrieveInternalGenericType(genericReturnType);
-                JavaType genericInternalType = null;
-                if (genericReturnType.getRawType() instanceof Class<?> genericPrimitiveType) {
-                    if (Collection.class.isAssignableFrom(genericPrimitiveType)) {
-                        genericInternalType = objectMapper.getTypeFactory()
-                                .constructCollectionType((Class<? extends Collection>) genericPrimitiveType,
-                                        actualGenericClass);
-                    } else {
-                        genericInternalType = objectMapper.getTypeFactory()
-                                .constructParametricType(genericPrimitiveType, actualGenericClass);
-                    }
-                }
-
-                return objectMapper.convertValue(cachedData, genericInternalType);
-            }
-
-            Object result = joinPoint.proceed();                        // Invoke Method
-
-            if (result != null) {
-                boolean shouldCache;
-                if (result instanceof Collection<?> collection) {
-                    shouldCache = !collection.isEmpty() || redisCache.cacheEmptyCollection();
-                } else {
-                    shouldCache = true;
-                }
-                if (shouldCache) {
-                    redisTemplate.opsForValue().set(redisKey, result, getTtlAsDuration(redisCache));
-                }
-            }
-
             return result;
         } catch (Throwable e) {
-            throw new IllegalArgumentException("cannot invoke method " + joinPoint.getSignature().getName(), e);
+            throw new IllegalArgumentException("Error executing method " + joinPoint.getSignature().getName());
         }
     }
 
-    private String getRedisKey(ProceedingJoinPoint joinPoint, RedisCache redisCache, MethodSignature methodSignature,
-                               OnSquadType onsquadType) {
-        Map<String, Object> parameterMap = mapParametersToValues(methodSignature, joinPoint.getArgs());
-        Object communityTypeId = retrieveIdentifier(redisCache.id(), parameterMap);
-        return String.format(onsquadType.getFormat(), communityTypeId, redisCache.name());
+    private String generateRedisKey(ProceedingJoinPoint joinPoint, RedisCache redisCache) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Map<String, Object> params = getMethodParams(methodSignature, joinPoint.getArgs());
+        String cacheKeyParam = getCacheKeyParam(redisCache.id(), params);
+        return String.format(redisCache.type().getFormat(), cacheKeyParam, redisCache.name());
     }
 
-    private Map<String, Object> mapParametersToValues(MethodSignature methodSignature, Object[] argumentValues) {
-        String[] parameterNames = methodSignature.getParameterNames();
-
-        return IntStream.range(0, parameterNames.length)
+    private Map<String, Object> getMethodParams(MethodSignature methodSignature, Object[] args) {
+        String[] paramNames = methodSignature.getParameterNames();
+        return IntStream.range(0, paramNames.length)
                 .boxed()
-                .collect(Collectors.toUnmodifiableMap(i -> parameterNames[i], i -> argumentValues[i]));
+                .collect(Collectors.toMap(i -> paramNames[i], i -> args[i]));
     }
 
-    private Object retrieveIdentifier(String parameter, Map<String, Object> parameterMap) {
-        Object communityTypeId = parameterMap.get(parameter);
-        if (parameterMap.get(parameter) == null) {
-            throw new IllegalArgumentException();
+    private String getCacheKeyParam(String paramName, Map<String, Object> params) {
+        Object paramValue = params.get(paramName);
+        if (paramValue == null) {
+            throw new IllegalArgumentException("Missing parameter: " + paramName);
+        }
+        return paramValue.toString();
+    }
+
+    private Object deserializeCachedData(ProceedingJoinPoint joinPoint, Object cachedData) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        ParameterizedType genericReturnType = resolveGenericReturnType(methodSignature);
+        if (genericReturnType == null) {
+            return objectMapper.convertValue(cachedData, methodSignature.getReturnType());
         }
 
-        return communityTypeId;
+        Type genericOuterClass = genericReturnType.getRawType();
+        Class<?> genericInternalClass = guessGenericInternalClass(genericReturnType);
+        JavaType javaType = determineJavaTypeForGeneric(genericOuterClass, genericInternalClass);
+        return objectMapper.convertValue(cachedData, javaType);
     }
 
-    private ParameterizedType resolveGenericReturnType(Method method) {
+    private ParameterizedType resolveGenericReturnType(MethodSignature methodSignature) {
+        Method method = methodSignature.getMethod();
         if (method.getGenericReturnType() instanceof ParameterizedType parameterizedType) {
             return parameterizedType;
         }
-
         return null;
     }
 
-    private Class<?> retrieveInternalGenericType(ParameterizedType genericReturnType) {
+    private Class<?> guessGenericInternalClass(ParameterizedType genericReturnType) {
         for (Type typeArgument : genericReturnType.getActualTypeArguments()) {
             if (typeArgument instanceof Class<?> actualType) {
                 return actualType;
             }
         }
-
         return null;
     }
 
-    private Duration getTtlAsDuration(RedisCache redisCache) {
-        long ttl = redisCache.ttl();
-        TimeUnit unit = redisCache.unit();
+    private JavaType determineJavaTypeForGeneric(Type genericOuterClass, Class<?> genericInternalClass) {
+        if (genericOuterClass instanceof Class<?> genericPrimitiveType) {
+            if (Collection.class.isAssignableFrom(genericPrimitiveType)) {
+                return objectMapper.getTypeFactory().constructCollectionType(
+                        (Class<? extends Collection>) genericPrimitiveType, genericInternalClass
+                );
+            }
+            if (Map.class.isAssignableFrom(genericPrimitiveType)) {
+                return objectMapper.getTypeFactory().constructMapType(
+                        (Class<? extends Map>) genericPrimitiveType, String.class, genericInternalClass
+                );
+            }
+            return objectMapper.getTypeFactory().constructParametricType(
+                    genericPrimitiveType, genericInternalClass
+            );
+        }
+        return null;
+    }
 
-        return switch (unit) {
-            case NANOSECONDS -> Duration.ofNanos(ttl);
-            case MICROSECONDS -> Duration.ofMillis(TimeUnit.MICROSECONDS.toMillis(ttl));
-            case MILLISECONDS -> Duration.ofMillis(ttl);
-            case SECONDS -> Duration.ofSeconds(ttl);
-            case MINUTES -> Duration.ofMinutes(ttl);
-            case HOURS -> Duration.ofHours(ttl);
-            case DAYS -> Duration.ofDays(ttl);
-        };
+    private boolean shouldCache(RedisCache redisCache, Object result) {
+        if (result == null) {
+            return false;
+        }
+        if (result instanceof Collection<?> collection) {
+            return !collection.isEmpty() || redisCache.cacheEmptyCollection();
+        }
+        return true;
+    }
+
+    private Duration calculateTtl(RedisCache redisCache) {
+        return Duration.of(redisCache.ttl(), redisCache.unit().toChronoUnit());
     }
 }
