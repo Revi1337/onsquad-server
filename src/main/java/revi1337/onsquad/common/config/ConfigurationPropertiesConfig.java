617d525ed44adcf251d28599c1d145b41f4b0888
Revi1337 <david122123@gmail.com>
2025-03-26 01:31:44 +0900

    remove: 불필요한 파일을 제거합니다

--- src/main/java/revi1337/onsquad/common/config/ConfigurationPropertiesConfig.java
+++
@@ -1,9 +0,0 @@
-package revi1337.onsquad.common.config;
-
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ConfigurationPropertiesScan(basePackages = "revi1337.onsquad")
-public class ConfigurationPropertiesConfig {
-}
