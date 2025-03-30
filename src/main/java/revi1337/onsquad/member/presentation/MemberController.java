a8a70c8f8bbe10caef96c6368fe9acbf65eb6e3c
Revi1337 <david122123@gmail.com>
2025-03-30 21:01:49 +0900

    refactor: 회원가입 절차 비지니스로직을 리팩토링합니다

--- src/main/java/revi1337/onsquad/member/presentation/MemberController.java
+++ src/main/java/revi1337/onsquad/member/presentation/MemberController.java
@@ -4,22 +4,21 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import revi1337.onsquad.auth.application.AuthMemberAttribute;
 import revi1337.onsquad.auth.config.Authenticate;
-import revi1337.onsquad.auth.presentation.dto.response.DuplicateNicknameResponse;
+import revi1337.onsquad.auth.presentation.dto.response.DuplicateResponse;
 import revi1337.onsquad.common.dto.RestResponse;
 import revi1337.onsquad.member.application.MemberService;
 import revi1337.onsquad.member.presentation.dto.request.MemberJoinRequest;
@@ -34,15 +33,26 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/members/check")
-    public ResponseEntity<RestResponse<DuplicateNicknameResponse>> checkDuplicateNickname(
-            @RequestParam @NotEmpty String nickname
+    @GetMapping("/members/verify/nickname/{nickname}")
+    public ResponseEntity<RestResponse<DuplicateResponse>> checkDuplicateNickname(
+            @PathVariable String nickname
     ) {
         if (memberService.checkDuplicateNickname(nickname)) {
-            return ResponseEntity.ok(RestResponse.success(DuplicateNicknameResponse.of(true)));
+            return ResponseEntity.ok(RestResponse.success(DuplicateResponse.of(true)));
         }
 
-        return ResponseEntity.ok(RestResponse.success(DuplicateNicknameResponse.of(false)));
+        return ResponseEntity.ok(RestResponse.success(DuplicateResponse.of(false)));
+    }
+
+    @GetMapping("/members/verify/email/{email}")
+    public ResponseEntity<RestResponse<DuplicateResponse>> checkDuplicateEmail(
+            @PathVariable String email
+    ) {
+        if (memberService.checkDuplicateEmail(email)) {
+            return ResponseEntity.ok(RestResponse.success(DuplicateResponse.of(true)));
+        }
+
+        return ResponseEntity.ok(RestResponse.success(DuplicateResponse.of(false)));
     }
 
     @PostMapping("/members")
@@ -65,7 +75,7 @@ public class MemberController {
         return ResponseEntity.ok().body(RestResponse.success(memberInfoResponse));
     }
 
-    @PutMapping("/my")
+    @PatchMapping("/my")
     public ResponseEntity<RestResponse<String>> updatePassword(
             @Valid @RequestBody MemberPasswordUpdateRequest request,
             @Authenticate AuthMemberAttribute authMemberAttribute
@@ -75,7 +85,7 @@ public class MemberController {
         return ResponseEntity.ok().body(RestResponse.noContent());
     }
 
-    @PatchMapping(value = "/my", consumes = {MULTIPART_FORM_DATA_VALUE, APPLICATION_JSON_VALUE})
+    @PutMapping(value = "/my", consumes = {MULTIPART_FORM_DATA_VALUE, APPLICATION_JSON_VALUE})
     public ResponseEntity<RestResponse<String>> updateMember(
             @Valid @RequestPart(name = "request") MemberUpdateRequest request,
             @RequestPart(name = "file", required = false) MultipartFile file,
