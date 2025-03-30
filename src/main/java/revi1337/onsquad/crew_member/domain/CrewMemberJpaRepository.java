4defde5d72b2e556e42d0800b5443dfa983b7298
Revi1337 <david122123@gmail.com>
2025-03-30 21:00:17 +0900

    remove: 사용되지 않는 메서드를 제거합니다

--- src/main/java/revi1337/onsquad/crew_member/domain/CrewMemberJpaRepository.java
+++ src/main/java/revi1337/onsquad/crew_member/domain/CrewMemberJpaRepository.java
@@ -9,9 +9,6 @@ public interface CrewMemberJpaRepository extends JpaRepository<CrewMember, Long>
     @Query("select cm from CrewMember as cm where cm.crew.id = :crewId and cm.member.id = :memberId")
     Optional<CrewMember> findByCrewIdAndMemberId(Long crewId, Long memberId);
 
-    @Query("select cm from CrewMember as cm inner join fetch cm.member as m where cm.crew.id = :crewId and cm.member.id = :memberId")
-    Optional<CrewMember> findWithMemberByCrewIdAndMemberId(Long crewId, Long memberId);
-
     boolean existsByMemberIdAndCrewId(Long memberId, Long crewId);
 
 }
