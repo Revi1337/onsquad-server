50e60071facb15a8d7fc60af650d6a84527cb0bb
Revi1337 <david122123@gmail.com>
2025-04-09 03:43:36 +0900

    remove: 불필요한 파일을 제거합니다

--- src/main/java/revi1337/onsquad/auth/presentation/dto/response/DuplicateResponse.java
+++
@@ -1,9 +0,0 @@
-package revi1337.onsquad.auth.presentation.dto.response;
-
-public record DuplicateResponse(
-        boolean duplicate
-) {
-    public static DuplicateResponse of(boolean duplicate) {
-        return new DuplicateResponse(duplicate);
-    }
-}
--- src/main/java/revi1337/onsquad/inrastructure/s3/application/FileUploadManager.java
+++
@@ -1,9 +0,0 @@
-package revi1337.onsquad.inrastructure.s3.application;
-
-public interface FileUploadManager {
-
-    String uploadFile(byte[] fileContent, String uploadUri, String fileName);
-
-    void deleteFile(String uploadUrl);
-
-}
