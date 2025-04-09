2912a63f889eee9c254159791f13a0dcd9b17686
Revi1337 <david122123@gmail.com>
2025-04-10 05:28:11 +0900

    refactor: 애플리케이션 초기화 관련 파일과 이벤트 관련 파일 패키지 구조를 변경합니다

---
+++ src/main/java/revi1337/onsquad/announce/application/event/listener/AnnounceFixedEventListener.java
@@ -0,0 +1,34 @@
+package revi1337.onsquad.announce.application.event.listener;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+import revi1337.onsquad.announce.application.event.AnnounceFixedEvent;
+import revi1337.onsquad.announce.domain.AnnounceRepository;
+import revi1337.onsquad.announce.domain.dto.AnnounceInfoDomainDto;
+import revi1337.onsquad.common.constant.CacheConst;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class AnnounceFixedEventListener {
+
+    private final AnnounceRepository announceRepository;
+    private final CacheManager caffeineCacheManager;
+
+    @TransactionalEventListener
+    public void handleAnnounceFixedEvent(AnnounceFixedEvent fixedEvent) {
+        log.debug("[{}] Renew fixed announces caches in crew_id = {}", fixedEvent.getEventName(), fixedEvent.crewId());
+        List<AnnounceInfoDomainDto> announceInfos = announceRepository
+                .findLimitedAnnouncesByCrewId(fixedEvent.crewId());
+
+        Cache cache = caffeineCacheManager.getCache(CacheConst.CREW_ANNOUNCES);
+        String computedCacheName = String.format("crew:%d", fixedEvent.crewId());
+        cache.evict(computedCacheName);
+        cache.put(computedCacheName, announceInfos);
+    }
+}
