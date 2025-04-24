7d528557a9a07d4f2487316184e473f755812940
Revi1337 <david122123@gmail.com>
2025-04-24 22:57:32 +0900

    test: 테스트에서 인증번호와 쓰레기통 동기화 작업이 이루어지지 않도록 수정합니다

--- src/test/java/revi1337/onsquad/OnsquadServerApplicationTests.java
+++ src/test/java/revi1337/onsquad/OnsquadServerApplicationTests.java
@@ -1,8 +1,10 @@
 package revi1337.onsquad;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class OnsquadServerApplicationTests {
 
