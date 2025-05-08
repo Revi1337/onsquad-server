ddbad3eef963ca1806d77b709940302b88dd8f2e
Revi1337 <david122123@gmail.com>
2025-05-08 17:57:29 +0900

    test: 카테고리 테스트를 작성합니다

---
+++ src/test/java/revi1337/onsquad/category/application/DefaultCategoryServiceTest.java
@@ -0,0 +1,28 @@
+package revi1337.onsquad.category.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+import revi1337.onsquad.category.domain.vo.CategoryType;
+import revi1337.onsquad.common.ApplicationLayerTestSupport;
+
+@Sql({"/h2-category.sql"})
+class DefaultCategoryServiceTest extends ApplicationLayerTestSupport {
+
+    @Autowired
+    private DefaultCategoryService defaultCategoryService;
+
+    @Test
+    @DisplayName("모든 Category 값들 조회에 성공한다.")
+    void success() {
+        List<String> categoryTexts = CategoryType.texts();
+
+        List<String> categories = defaultCategoryService.findCategories();
+
+        assertThat(categories).containsExactlyInAnyOrder(categoryTexts.toArray(String[]::new));
+    }
+}
