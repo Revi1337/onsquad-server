bdf455595bc3ea9e5b8761585adb356966cad2e1
Revi1337 <david122123@gmail.com>
2025-05-13 10:49:30 +0900

    style: 크루 비즈니스 로직 및 테스트를 통합합니다

--- src/main/java/revi1337/onsquad/crew/presentation/CrewController.java
+++ src/main/java/revi1337/onsquad/crew/presentation/CrewController.java
@@ -1,19 +1,33 @@
 package revi1337.onsquad.crew.presentation;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
+
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import revi1337.onsquad.auth.application.AuthMemberAttribute;
 import revi1337.onsquad.auth.config.Authenticate;
 import revi1337.onsquad.common.dto.RestResponse;
+import revi1337.onsquad.crew.application.CrewCommandExecutor;
 import revi1337.onsquad.crew.application.CrewQueryService;
+import revi1337.onsquad.crew.presentation.dto.request.CrewCreateRequest;
+import revi1337.onsquad.crew.presentation.dto.request.CrewUpdateRequest;
 import revi1337.onsquad.crew.presentation.dto.response.CrewInfoResponse;
 import revi1337.onsquad.crew.presentation.dto.response.DuplicateCrewNameResponse;
 import revi1337.onsquad.crew_member.presentation.dto.response.EnrolledCrewResponse;
@@ -21,8 +35,9 @@ import revi1337.onsquad.crew_member.presentation.dto.response.EnrolledCrewRespon
 @RequiredArgsConstructor
 @RequestMapping("/api/crews")
 @RestController
-public class CrewQueryController {
+public class CrewController {
 
+    private final CrewCommandExecutor crewCommandExecutor;
     private final CrewQueryService crewQueryService;
 
     @GetMapping("/check")
@@ -35,6 +50,17 @@ public class CrewQueryController {
         }
 
         return ResponseEntity.ok().body(RestResponse.success(DuplicateCrewNameResponse.of(false)));
+    }
+
+    @PostMapping(consumes = {MULTIPART_FORM_DATA_VALUE, APPLICATION_JSON_VALUE})
+    public ResponseEntity<RestResponse<String>> newCrew(
+            @Valid @RequestPart CrewCreateRequest request,
+            @RequestPart(required = false) MultipartFile file,
+            @Authenticate AuthMemberAttribute authMemberAttribute
+    ) {
+        crewCommandExecutor.newCrew(authMemberAttribute.id(), request.toDto(), file);
+
+        return ResponseEntity.ok().body(RestResponse.created());
     }
 
     @GetMapping("/{crewId}")
@@ -50,6 +76,48 @@ public class CrewQueryController {
 
         crewResponse = CrewInfoResponse.from(crewQueryService.findCrewById(authMemberAttribute.id(), crewId));
         return ResponseEntity.ok().body(RestResponse.success(crewResponse));
+    }
+
+    @PutMapping("/{crewId}")
+    public ResponseEntity<RestResponse<String>> updateCrew(
+            @PathVariable Long crewId,
+            @Valid @RequestBody CrewUpdateRequest crewUpdateRequest,
+            @Authenticate AuthMemberAttribute authMemberAttribute
+    ) {
+        crewCommandExecutor.updateCrew(authMemberAttribute.id(), crewId, crewUpdateRequest.toDto());
+
+        return ResponseEntity.ok().body(RestResponse.noContent());
+    }
+
+    @DeleteMapping("/{crewId}")
+    public ResponseEntity<RestResponse<String>> deleteCrew(
+            @PathVariable Long crewId,
+            @Authenticate AuthMemberAttribute authMemberAttribute
+    ) {
+        crewCommandExecutor.deleteCrew(authMemberAttribute.id(), crewId);
+
+        return ResponseEntity.ok().body(RestResponse.noContent());
+    }
+
+    @PatchMapping(value = "/{crewId}/image", consumes = MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<RestResponse<String>> updateCrewImage(
+            @PathVariable Long crewId,
+            @RequestPart MultipartFile file,
+            @Authenticate AuthMemberAttribute authMemberAttribute
+    ) {
+        crewCommandExecutor.updateCrewImage(authMemberAttribute.id(), crewId, file);
+
+        return ResponseEntity.ok().body(RestResponse.noContent());
+    }
+
+    @DeleteMapping("/{crewId}/image")
+    public ResponseEntity<RestResponse<String>> deleteCrewImage(
+            @PathVariable Long crewId,
+            @Authenticate AuthMemberAttribute authMemberAttribute
+    ) {
+        crewCommandExecutor.deleteCrewImage(authMemberAttribute.id(), crewId);
+
+        return ResponseEntity.ok().body(RestResponse.noContent());
     }
 
     @GetMapping
