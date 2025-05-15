042cbd70ea2e60620c3ec09d6040944aa58e1eb8
Revi1337 <david122123@gmail.com>
2025-05-15 17:25:47 +0900

    fix: 크루 공지사항 조회 책임 위임 테스트를 수정합니다

--- src/test/java/revi1337/onsquad/announce/domain/AnnounceRepositoryImplTest.java
+++ src/test/java/revi1337/onsquad/announce/domain/AnnounceRepositoryImplTest.java
@@ -1,39 +1,42 @@
 package revi1337.onsquad.announce.domain;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static revi1337.onsquad.common.fixture.AnnounceFixture.ANNOUNCE;
 
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageRequest;
 import revi1337.onsquad.announce.domain.dto.AnnounceDomainDto;
-import revi1337.onsquad.common.PersistenceLayerTestSupport;
+import revi1337.onsquad.announce.error.exception.AnnounceBusinessException;
 
-@Import({AnnounceRepositoryImpl.class})
-class AnnounceRepositoryImplTest extends PersistenceLayerTestSupport {
+@ExtendWith(MockitoExtension.class)
+class AnnounceRepositoryImplTest {
 
-    @MockBean
-    private AnnounceCacheRepository announceCacheRepository;
-
-    @MockBean
+    @Mock
     private AnnounceJpaRepository announceJpaRepository;
 
-    @MockBean
+    @Mock
     private AnnounceQueryDslRepository announceQueryDslRepository;
 
-    @Autowired
-    private AnnounceRepository announceRepository;
+    @Mock
+    private AnnounceCacheRepository announceCacheRepository;
+
+    @InjectMocks
+    private AnnounceRepositoryImpl announceRepository;
 
     @Test
     @DisplayName("save 위임에 성공한다.")
-    void success1() {
-        Announce ANNOUNCE = ANNOUNCE();
+    void save() {
+        Announce ANNOUNCE = mock(Announce.class);
 
         announceRepository.save(ANNOUNCE);
 
@@ -42,44 +45,75 @@ class AnnounceRepositoryImplTest extends PersistenceLayerTestSupport {
 
     @Test
     @DisplayName("saveAndFlush 위임에 성공한다.")
-    void success2() {
-        Announce ANNOUNCE = ANNOUNCE();
+    void saveAndFlush() {
+        Announce ANNOUNCE = mock(Announce.class);
 
         announceRepository.saveAndFlush(ANNOUNCE);
 
         verify(announceJpaRepository).saveAndFlush(ANNOUNCE);
     }
 
-    @Test
-    @DisplayName("findByIdAndCrewId 위임에 성공한다.")
-    void success3() {
-        Long DUMMY_ANNOUNCE_ID = 1L;
-        Long DUMMY_CREW_ID = 1L;
+    @Nested
+    @DisplayName("findByIdAndCrewId & getByIdAndCrewId 위임을 테스트한다.")
+    class FindAndGetByIdAndCrewId {
 
-        announceRepository.findByIdAndCrewId(DUMMY_ANNOUNCE_ID, DUMMY_CREW_ID);
+        @Test
+        @DisplayName("findByIdAndCrewId 위임에 성공한다.")
+        void findByIdAndCrewId() {
+            Long DUMMY_ANNOUNCE_ID = 1L;
+            Long DUMMY_CREW_ID = 1L;
 
-        verify(announceJpaRepository).findByIdAndCrewId(DUMMY_ANNOUNCE_ID, DUMMY_CREW_ID);
+            announceRepository.findByIdAndCrewId(DUMMY_ANNOUNCE_ID, DUMMY_CREW_ID);
+
+            verify(announceJpaRepository).findByIdAndCrewId(DUMMY_ANNOUNCE_ID, DUMMY_CREW_ID);
+        }
+
+        @Test
+        @DisplayName("getByIdAndCrewId 위임에 성공한다.")
+        void getByIdAndCrewId() {
+            Long DUMMY_ANNOUNCE_ID = 1L;
+            Long DUMMY_CREW_ID = 1L;
+            Announce ANNOUNCE = mock(Announce.class);
+            when(announceJpaRepository.findByIdAndCrewId(DUMMY_ANNOUNCE_ID, DUMMY_CREW_ID))
+                    .thenReturn(Optional.of(ANNOUNCE));
+
+            announceRepository.findByIdAndCrewId(DUMMY_ANNOUNCE_ID, DUMMY_CREW_ID);
+
+            verify(announceJpaRepository).findByIdAndCrewId(DUMMY_ANNOUNCE_ID, DUMMY_CREW_ID);
+        }
+
+        @Test
+        @DisplayName("getByIdAndCrewId 를 호출했을 때, findByIdAndCrewId 의 결과가 없으면 실패한다.")
+        void getByIdAndCrewIdFail() {
+            Long DUMMY_ANNOUNCE_ID = 1L;
+            Long DUMMY_CREW_ID = 1L;
+            when(announceJpaRepository.findByIdAndCrewId(DUMMY_ANNOUNCE_ID, DUMMY_CREW_ID))
+                    .thenReturn(Optional.empty());
+
+            assertThatThrownBy(() -> announceRepository.getByIdAndCrewId(DUMMY_ANNOUNCE_ID, DUMMY_CREW_ID))
+                    .isExactlyInstanceOf(AnnounceBusinessException.NotFoundById.class);
+        }
     }
 
     @Test
     @DisplayName("fetchAllByCrewId 위임에 성공한다.")
-    void success4() {
+    void fetchAllByCrewId() {
         Long DUMMY_CREW_ID = 1L;
-        PageRequest PAGE_REQEST = PageRequest.of(0, 10);
+        PageRequest PAGE_REQUEST = mock(PageRequest.class);
 
-        announceRepository.fetchAllByCrewId(DUMMY_CREW_ID, PAGE_REQEST);
+        announceRepository.fetchAllByCrewId(DUMMY_CREW_ID, PAGE_REQUEST);
 
-        verify(announceQueryDslRepository).fetchAllByCrewId(DUMMY_CREW_ID, PAGE_REQEST);
+        verify(announceQueryDslRepository).fetchAllByCrewId(DUMMY_CREW_ID, PAGE_REQUEST);
     }
 
     @Test
     @DisplayName("fetchCacheByCrewIdAndId 위임에 성공한다.")
-    void success5() {
+    void fetchCacheByCrewIdAndId() {
         Long DUMMY_CREW_ID = 1L;
         Long DUMMY_ANNOUNCE_ID = 1L;
-        AnnounceDomainDto MOCKED = mock(AnnounceDomainDto.class);
+        AnnounceDomainDto DOMAIN_DTO = mock(AnnounceDomainDto.class);
         when(announceCacheRepository.fetchCacheByCrewIdAndId(anyLong(), anyLong()))
-                .thenReturn(MOCKED);
+                .thenReturn(DOMAIN_DTO);
 
         announceRepository.fetchCacheByCrewIdAndId(DUMMY_CREW_ID, DUMMY_ANNOUNCE_ID);
 
