8fee0e681bf29f9dcd90a0e2eb02d9fadaaa8830
Revi1337 <david122123@gmail.com>
2025-05-16 01:37:58 +0900

    test: 크루 공지사항 삭제 및 업데이트 테스트를 작성합니다

--- src/test/java/revi1337/onsquad/announce/application/AnnounceCommandServiceTest.java
+++ src/test/java/revi1337/onsquad/announce/application/AnnounceCommandServiceTest.java
@@ -10,7 +10,10 @@ import static revi1337.onsquad.common.constant.CacheConst.CREW_ANNOUNCES;
 import static revi1337.onsquad.common.fixture.AnnounceFixture.ANNOUNCE;
 import static revi1337.onsquad.common.fixture.AnnounceFixture.FIXED_ANNOUNCE;
 import static revi1337.onsquad.common.fixture.AnnounceValueFixture.ANNOUNCE_CONTENT_VALUE;
+import static revi1337.onsquad.common.fixture.AnnounceValueFixture.ANNOUNCE_CONTENT_VALUE_1;
+import static revi1337.onsquad.common.fixture.AnnounceValueFixture.ANNOUNCE_TITLE_1;
 import static revi1337.onsquad.common.fixture.AnnounceValueFixture.ANNOUNCE_TITLE_VALUE;
+import static revi1337.onsquad.common.fixture.AnnounceValueFixture.ANNOUNCE_TITLE_VALUE_1;
 import static revi1337.onsquad.common.fixture.CrewFixture.CREW;
 import static revi1337.onsquad.common.fixture.CrewMemberFixture.GENERAL_CREW_MEMBER;
 import static revi1337.onsquad.common.fixture.CrewMemberFixture.MANAGER_CREW_MEMBER;
@@ -18,6 +21,7 @@ import static revi1337.onsquad.common.fixture.MemberFixtures.ANDONG;
 import static revi1337.onsquad.common.fixture.MemberFixtures.KWANGWON;
 import static revi1337.onsquad.common.fixture.MemberFixtures.REVI;
 
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -27,8 +31,10 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.test.context.event.ApplicationEvents;
 import revi1337.onsquad.announce.application.dto.AnnounceCreateDto;
+import revi1337.onsquad.announce.application.dto.AnnounceUpdateDto;
 import revi1337.onsquad.announce.application.event.AnnounceCreateEvent;
 import revi1337.onsquad.announce.application.event.AnnounceFixedEvent;
+import revi1337.onsquad.announce.application.event.AnnounceUpdateEvent;
 import revi1337.onsquad.announce.domain.Announce;
 import revi1337.onsquad.announce.domain.AnnounceCacheRepository;
 import revi1337.onsquad.announce.domain.AnnounceJpaRepository;
@@ -39,6 +45,7 @@ import revi1337.onsquad.crew.domain.Crew;
 import revi1337.onsquad.crew.domain.CrewRepository;
 import revi1337.onsquad.crew_member.domain.CrewMember;
 import revi1337.onsquad.crew_member.domain.CrewMemberRepository;
+import revi1337.onsquad.crew_member.error.exception.CrewMemberBusinessException;
 import revi1337.onsquad.member.domain.Member;
 import revi1337.onsquad.member.domain.MemberRepository;
 
@@ -87,6 +94,7 @@ class AnnounceCommandServiceTest extends ApplicationLayerTestSupport {
             Member REVI = memberRepository.save(REVI());
             Crew CREW = crewRepository.save(CREW(REVI));
             AnnounceCreateDto DTO = new AnnounceCreateDto(ANNOUNCE_TITLE_VALUE, ANNOUNCE_CONTENT_VALUE);
+            clearPersistenceContext();
 
             Long ANNOUNCE_ID = announceCommandService.newAnnounce(REVI.getId(), CREW.getId(), DTO);
 
@@ -95,17 +103,168 @@ class AnnounceCommandServiceTest extends ApplicationLayerTestSupport {
         }
 
         @Test
-        @DisplayName("Crew 에 속한 사용자가 매니저 혹은 크루장이 아니면 실패한다.")
+        @DisplayName("크루장 또는 크루매니저가 아니면 실패한다.")
         void fail() {
             Member REVI = memberRepository.save(REVI());
             Crew CREW = crewRepository.save(CREW(REVI));
             Member ANDONG = memberRepository.save(ANDONG());
             crewMemberRepository.save(GENERAL_CREW_MEMBER(CREW, ANDONG));
             AnnounceCreateDto DTO = new AnnounceCreateDto(ANNOUNCE_TITLE_VALUE, ANNOUNCE_CONTENT_VALUE);
+            clearPersistenceContext();
 
             assertThatThrownBy(() -> announceCommandService.newAnnounce(ANDONG.getId(), CREW.getId(), DTO))
-                    .isExactlyInstanceOf(AnnounceBusinessException.CantMake.class);
+                    .isExactlyInstanceOf(CrewMemberBusinessException.LessThenManager.class);
             assertThat(events.stream(AnnounceCreateEvent.class)).hasSize(0);
+        }
+    }
+
+    @Nested
+    @DisplayName("Announce 업데이트를 테스트한다.")
+    class Update {
+
+        @Test
+        @DisplayName("Announce 업데이트를 성공한다.")
+        void success1() {
+            Member REVI = memberRepository.save(REVI());
+            Crew CREW = crewRepository.save(CREW(REVI));
+            CrewMember CREW_OWNER = crewMemberRepository.findByCrewIdAndMemberId(CREW.getId(), REVI.getId()).get();
+            Announce ANNOUNCE = announceJpaRepository.save(ANNOUNCE(CREW, CREW_OWNER));
+            AnnounceUpdateDto DTO = new AnnounceUpdateDto(ANNOUNCE_TITLE_VALUE_1, ANNOUNCE_CONTENT_VALUE_1);
+            clearPersistenceContext();
+
+            announceCommandService.updateAnnounce(REVI.getId(), CREW.getId(), ANNOUNCE.getId(), DTO);
+
+            clearPersistenceContext();
+            Optional<Announce> announce = announceJpaRepository.findById(ANNOUNCE.getId());
+            assertThat(announce).isPresent();
+            assertThat(announce.get().getTitle()).isEqualTo(ANNOUNCE_TITLE_1);
+            assertThat(announce.get().getContent()).isEqualTo(ANNOUNCE_CONTENT_VALUE_1);
+            assertThat(events.stream(AnnounceUpdateEvent.class)).hasSize(1);
+        }
+
+        @Test
+        @DisplayName("크루장은 크루매니저가 작성한 공지사항을 업데이트할 수 있다.")
+        void success2() {
+            Member REVI = memberRepository.save(REVI());
+            Crew CREW = crewRepository.save(CREW(REVI));
+            Member ANDONG = memberRepository.save(ANDONG());
+            CrewMember ANDONG_MANAGER = crewMemberRepository.save(MANAGER_CREW_MEMBER(CREW, ANDONG));
+            Announce ANNOUNCE = announceJpaRepository.save(ANNOUNCE(CREW, ANDONG_MANAGER));
+            AnnounceUpdateDto DTO = new AnnounceUpdateDto(ANNOUNCE_TITLE_VALUE_1, ANNOUNCE_CONTENT_VALUE_1);
+            clearPersistenceContext();
+
+            announceCommandService.updateAnnounce(REVI.getId(), CREW.getId(), ANNOUNCE.getId(), DTO);
+
+            clearPersistenceContext();
+            Optional<Announce> announce = announceJpaRepository.findById(ANNOUNCE.getId());
+            assertThat(announce).isPresent();
+            assertThat(announce.get().getTitle()).isEqualTo(ANNOUNCE_TITLE_1);
+            assertThat(announce.get().getContent()).isEqualTo(ANNOUNCE_CONTENT_VALUE_1);
+            assertThat(events.stream(AnnounceUpdateEvent.class)).hasSize(1);
+        }
+
+        @Test
+        @DisplayName("크루장 또는 크루매니저가 아니면 업데이트에 실패한다.")
+        void fail1() {
+            Member REVI = memberRepository.save(REVI());
+            Crew CREW = crewRepository.save(CREW(REVI));
+            CrewMember CREW_OWNER = crewMemberRepository.findByCrewIdAndMemberId(CREW.getId(), REVI.getId()).get();
+            Announce ANNOUNCE = announceJpaRepository.save(ANNOUNCE(CREW, CREW_OWNER));
+            Member ANDONG = memberRepository.save(ANDONG());
+            crewMemberRepository.save(GENERAL_CREW_MEMBER(CREW, ANDONG));
+            AnnounceUpdateDto DTO = new AnnounceUpdateDto(ANNOUNCE_TITLE_VALUE_1, ANNOUNCE_CONTENT_VALUE_1);
+            clearPersistenceContext();
+
+            assertThatThrownBy(() -> announceCommandService
+                    .updateAnnounce(ANDONG.getId(), CREW.getId(), ANNOUNCE.getId(), DTO))
+                    .isExactlyInstanceOf(CrewMemberBusinessException.LessThenManager.class);
+        }
+
+        @Test
+        @DisplayName("크루매니저가 크루장의 공지사항을 업데이트하려하면 실패한다.")
+        void fail2() {
+            Member REVI = memberRepository.save(REVI());
+            Crew CREW = crewRepository.save(CREW(REVI));
+            CrewMember CREW_OWNER = crewMemberRepository.findByCrewIdAndMemberId(CREW.getId(), REVI.getId()).get();
+            Announce ANNOUNCE = announceJpaRepository.save(ANNOUNCE(CREW, CREW_OWNER));
+            Member ANDONG = memberRepository.save(ANDONG());
+            crewMemberRepository.save(MANAGER_CREW_MEMBER(CREW, ANDONG));
+            AnnounceUpdateDto DTO = new AnnounceUpdateDto(ANNOUNCE_TITLE_VALUE_1, ANNOUNCE_CONTENT_VALUE_1);
+            clearPersistenceContext();
+
+            assertThatThrownBy(() -> announceCommandService
+                    .updateAnnounce(ANDONG.getId(), CREW.getId(), ANNOUNCE.getId(), DTO))
+                    .isExactlyInstanceOf(AnnounceBusinessException.InvalidReference.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("Announce 삭제를 테스트한다.")
+    class Delete {
+
+        @Test
+        @DisplayName("Announce 삭제에 성공한다.")
+        void success1() {
+            Member REVI = memberRepository.save(REVI());
+            Crew CREW = crewRepository.save(CREW(REVI));
+            CrewMember CREW_OWNER = crewMemberRepository.findByCrewIdAndMemberId(CREW.getId(), REVI.getId()).get();
+            Announce ANNOUNCE = announceJpaRepository.save(ANNOUNCE(CREW, CREW_OWNER));
+            Long ANNOUNCE_ID = ANNOUNCE.getId();
+            clearPersistenceContext();
+
+            announceCommandService.deleteAnnounce(REVI.getId(), CREW.getId(), ANNOUNCE.getId());
+
+            clearPersistenceContext();
+            assertThat(announceJpaRepository.findById(ANNOUNCE_ID)).isEmpty();
+        }
+
+        @Test
+        @DisplayName("크루장은 크루매니저가 작성한 공지사항을 삭제할 수 있다.")
+        void success2() {
+            Member REVI = memberRepository.save(REVI());
+            Crew CREW = crewRepository.save(CREW(REVI));
+            Member ANDONG = memberRepository.save(ANDONG());
+            CrewMember ANDONG_MANAGER = crewMemberRepository.save(MANAGER_CREW_MEMBER(CREW, ANDONG));
+            Announce ANNOUNCE = announceJpaRepository.save(ANNOUNCE(CREW, ANDONG_MANAGER));
+            Long ANNOUNCE_ID = ANNOUNCE.getId();
+            clearPersistenceContext();
+
+            announceCommandService.deleteAnnounce(REVI.getId(), CREW.getId(), ANNOUNCE.getId());
+
+            clearPersistenceContext();
+            assertThat(announceJpaRepository.findById(ANNOUNCE_ID)).isEmpty();
+        }
+
+        @Test
+        @DisplayName("크루장 또는 크루매니저가 아니면 삭제에 실패한다.")
+        void fail1() {
+            Member REVI = memberRepository.save(REVI());
+            Crew CREW = crewRepository.save(CREW(REVI));
+            CrewMember CREW_OWNER = crewMemberRepository.findByCrewIdAndMemberId(CREW.getId(), REVI.getId()).get();
+            Announce ANNOUNCE = announceJpaRepository.save(ANNOUNCE(CREW, CREW_OWNER));
+            Member ANDONG = memberRepository.save(ANDONG());
+            crewMemberRepository.save(GENERAL_CREW_MEMBER(CREW, ANDONG));
+            clearPersistenceContext();
+
+            assertThatThrownBy(() -> announceCommandService
+                    .deleteAnnounce(ANDONG.getId(), CREW.getId(), ANNOUNCE.getId()))
+                    .isExactlyInstanceOf(CrewMemberBusinessException.LessThenManager.class);
+        }
+
+        @Test
+        @DisplayName("크루매니저가 크루장의 공지사항을 업데이트하려하면 실패한다.")
+        void fail2() {
+            Member REVI = memberRepository.save(REVI());
+            Crew CREW = crewRepository.save(CREW(REVI));
+            CrewMember CREW_OWNER = crewMemberRepository.findByCrewIdAndMemberId(CREW.getId(), REVI.getId()).get();
+            Announce ANNOUNCE = announceJpaRepository.save(ANNOUNCE(CREW, CREW_OWNER));
+            Member ANDONG = memberRepository.save(ANDONG());
+            crewMemberRepository.save(MANAGER_CREW_MEMBER(CREW, ANDONG));
+            clearPersistenceContext();
+
+            assertThatThrownBy(() -> announceCommandService
+                    .deleteAnnounce(ANDONG.getId(), CREW.getId(), ANNOUNCE.getId()))
+                    .isExactlyInstanceOf(AnnounceBusinessException.InvalidReference.class);
         }
     }
 
@@ -120,6 +279,7 @@ class AnnounceCommandServiceTest extends ApplicationLayerTestSupport {
             Crew CREW = crewRepository.save(CREW(REVI));
             CrewMember CREW_OWNER = crewMemberRepository.getByCrewIdAndMemberId(CREW.getId(), REVI.getId());
             Announce ANNOUNCE = announceJpaRepository.save(ANNOUNCE(CREW, CREW_OWNER));
+            clearPersistenceContext();
 
             announceCommandService.fixOrUnfixAnnounce(CREW_OWNER.getId(), CREW.getId(), ANNOUNCE.getId(), true);
 
@@ -138,6 +298,7 @@ class AnnounceCommandServiceTest extends ApplicationLayerTestSupport {
             CrewMember CREW_MANAGER = crewMemberRepository.save(MANAGER_CREW_MEMBER(CREW, ANDONG));
             Announce ANNOUNCE = announceJpaRepository.save(ANNOUNCE(CREW, CREW_MANAGER));
             CrewMember CREW_OWNER = crewMemberRepository.getByCrewIdAndMemberId(CREW.getId(), REVI.getId());
+            clearPersistenceContext();
 
             announceCommandService.fixOrUnfixAnnounce(CREW_OWNER.getId(), CREW.getId(), ANNOUNCE.getId(), true);
 
@@ -157,6 +318,7 @@ class AnnounceCommandServiceTest extends ApplicationLayerTestSupport {
             CrewMember CREW_MANAGER = crewMemberRepository.save(MANAGER_CREW_MEMBER(CREW, ANDONG));
             Announce ANNOUNCE = announceJpaRepository.save(FIXED_ANNOUNCE(CREW, CREW_MANAGER));
             CrewMember CREW_OWNER = crewMemberRepository.getByCrewIdAndMemberId(CREW.getId(), REVI.getId());
+            clearPersistenceContext();
 
             announceCommandService.fixOrUnfixAnnounce(CREW_OWNER.getId(), CREW.getId(), ANNOUNCE.getId(), true);
 
@@ -175,10 +337,11 @@ class AnnounceCommandServiceTest extends ApplicationLayerTestSupport {
             Announce ANNOUNCE = announceJpaRepository.save(ANNOUNCE(CREW, CREW_MANAGER));
             Member ANDONG = memberRepository.save(ANDONG());
             CrewMember CREW_GENERAL = crewMemberRepository.save(GENERAL_CREW_MEMBER(CREW, ANDONG));
+            clearPersistenceContext();
 
             assertThatThrownBy(() -> announceCommandService
                     .fixOrUnfixAnnounce(CREW_GENERAL.getId(), CREW.getId(), ANNOUNCE.getId(), true))
-                    .isExactlyInstanceOf(AnnounceBusinessException.CantFix.class);
+                    .isExactlyInstanceOf(CrewMemberBusinessException.NotOwner.class);
         }
 
         @Test
@@ -189,10 +352,11 @@ class AnnounceCommandServiceTest extends ApplicationLayerTestSupport {
             Member KWANGWON = memberRepository.save(KWANGWON());
             CrewMember CREW_MANAGER = crewMemberRepository.save(MANAGER_CREW_MEMBER(CREW, KWANGWON));
             Announce ANNOUNCE = announceJpaRepository.save(ANNOUNCE(CREW, CREW_MANAGER));
+            clearPersistenceContext();
 
             assertThatThrownBy(() -> announceCommandService
                     .fixOrUnfixAnnounce(CREW_MANAGER.getId(), CREW.getId(), ANNOUNCE.getId(), true))
-                    .isExactlyInstanceOf(AnnounceBusinessException.CantFix.class);
+                    .isExactlyInstanceOf(CrewMemberBusinessException.NotOwner.class);
         }
     }
 
@@ -207,6 +371,7 @@ class AnnounceCommandServiceTest extends ApplicationLayerTestSupport {
             Crew CREW = crewRepository.save(CREW(REVI));
             CrewMember CREW_OWNER = crewMemberRepository.getByCrewIdAndMemberId(CREW.getId(), REVI.getId());
             Announce ANNOUNCE = announceJpaRepository.save(FIXED_ANNOUNCE(CREW, CREW_OWNER));
+            clearPersistenceContext();
 
             announceCommandService.fixOrUnfixAnnounce(CREW_OWNER.getId(), CREW.getId(), ANNOUNCE.getId(), false);
 
@@ -225,6 +390,7 @@ class AnnounceCommandServiceTest extends ApplicationLayerTestSupport {
             CrewMember CREW_MANAGER = crewMemberRepository.save(MANAGER_CREW_MEMBER(CREW, ANDONG));
             Announce ANNOUNCE = announceJpaRepository.save(FIXED_ANNOUNCE(CREW, CREW_MANAGER));
             CrewMember CREW_OWNER = crewMemberRepository.getByCrewIdAndMemberId(CREW.getId(), REVI.getId());
+            clearPersistenceContext();
 
             announceCommandService.fixOrUnfixAnnounce(CREW_OWNER.getId(), CREW.getId(), ANNOUNCE.getId(), false);
 
@@ -243,6 +409,7 @@ class AnnounceCommandServiceTest extends ApplicationLayerTestSupport {
             CrewMember CREW_MANAGER = crewMemberRepository.save(MANAGER_CREW_MEMBER(CREW, ANDONG));
             Announce ANNOUNCE = announceJpaRepository.save(ANNOUNCE(CREW, CREW_MANAGER));
             CrewMember CREW_OWNER = crewMemberRepository.getByCrewIdAndMemberId(CREW.getId(), REVI.getId());
+            clearPersistenceContext();
 
             announceCommandService.fixOrUnfixAnnounce(CREW_OWNER.getId(), CREW.getId(), ANNOUNCE.getId(), false);
 
@@ -261,10 +428,11 @@ class AnnounceCommandServiceTest extends ApplicationLayerTestSupport {
             Announce ANNOUNCE = announceJpaRepository.save(FIXED_ANNOUNCE(CREW, CREW_MANAGER));
             Member ANDONG = memberRepository.save(ANDONG());
             CrewMember CREW_GENERAL = crewMemberRepository.save(GENERAL_CREW_MEMBER(CREW, ANDONG));
+            clearPersistenceContext();
 
             assertThatThrownBy(() -> announceCommandService
                     .fixOrUnfixAnnounce(CREW_GENERAL.getId(), CREW.getId(), ANNOUNCE.getId(), false))
-                    .isExactlyInstanceOf(AnnounceBusinessException.CantFix.class);
+                    .isExactlyInstanceOf(CrewMemberBusinessException.NotOwner.class);
         }
 
         @Test
@@ -275,10 +443,11 @@ class AnnounceCommandServiceTest extends ApplicationLayerTestSupport {
             Member KWANGWON = memberRepository.save(KWANGWON());
             CrewMember CREW_MANAGER = crewMemberRepository.save(MANAGER_CREW_MEMBER(CREW, KWANGWON));
             Announce ANNOUNCE = announceJpaRepository.save(FIXED_ANNOUNCE(CREW, CREW_MANAGER));
+            clearPersistenceContext();
 
             assertThatThrownBy(() -> announceCommandService
                     .fixOrUnfixAnnounce(CREW_MANAGER.getId(), CREW.getId(), ANNOUNCE.getId(), false))
-                    .isExactlyInstanceOf(AnnounceBusinessException.CantFix.class);
+                    .isExactlyInstanceOf(CrewMemberBusinessException.NotOwner.class);
         }
     }
 }
