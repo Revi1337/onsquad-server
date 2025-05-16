3d023fc73e529ae88b3f6d43111e5b506a393785
Revi1337 <david122123@gmail.com>
2025-05-16 21:34:24 +0900

    refactor: 불필요한 쿼리를 제거합니다

--- src/main/java/revi1337/onsquad/squad/application/SquadCommandService.java
+++ src/main/java/revi1337/onsquad/squad/application/SquadCommandService.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import revi1337.onsquad.category.domain.Category;
-import revi1337.onsquad.crew.domain.Crew;
-import revi1337.onsquad.crew.domain.CrewRepository;
 import revi1337.onsquad.crew_member.domain.CrewMember;
 import revi1337.onsquad.crew_member.domain.CrewMemberRepository;
 import revi1337.onsquad.squad.application.dto.SquadCreateDto;
@@ -20,13 +18,13 @@ public class SquadCommandService {
 
     private final SquadRepository squadRepository;
     private final CrewMemberRepository crewMemberRepository;
-    private final CrewRepository crewRepository;
     private final SquadCategoryJdbcRepository squadCategoryJdbcRepository;
 
     public Long newSquad(Long memberId, Long crewId, SquadCreateDto dto) {
-        Crew crew = crewRepository.getById(crewId);
         CrewMember crewMember = crewMemberRepository.getByCrewIdAndMemberId(crewId, memberId);
-        Squad persistSquad = squadRepository.save(Squad.create(dto.toEntityMetadata(), crewMember, crew));
+        Squad persistSquad = squadRepository.save(
+                Squad.create(dto.toEntityMetadata(), crewMember, crewMember.getCrew())
+        );
         squadCategoryJdbcRepository.batchInsert(persistSquad.getId(), Category.fromCategoryTypes(dto.categories()));
 
         return persistSquad.getId();
