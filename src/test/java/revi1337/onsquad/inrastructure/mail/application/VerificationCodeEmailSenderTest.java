871d423744b669f17242d5171348216b9c5dc7c0
Revi1337 <david122123@gmail.com>
2025-07-28 15:57:08 +0900

    test: 인증코드 발송 예외처리 테스트를 작성합니다

--- src/test/java/revi1337/onsquad/inrastructure/mail/application/VerificationCodeEmailSenderTest.java
+++ src/test/java/revi1337/onsquad/inrastructure/mail/application/VerificationCodeEmailSenderTest.java
@@ -1,6 +1,7 @@
 package revi1337.onsquad.inrastructure.mail.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doThrow;
@@ -23,7 +24,9 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
+import revi1337.onsquad.inrastructure.mail.error.exception.EmailException;
 
 @ExtendWith(MockitoExtension.class)
 class VerificationCodeEmailSenderTest {
@@ -50,15 +53,26 @@ class VerificationCodeEmailSenderTest {
     }
 
     @Test
-    @DisplayName("MIME 설정 중 예외가 발생하면 인증코드를 발송하지 않는다.")
+    @DisplayName("MIME 설정 중 예외가 발생하면 인증코드를 발송하지 않고, 커스텀 예외가 발생한다.")
     void fail1() throws Exception {
         MimeMessage mockMimeMessage = mock(MimeMessage.class);
         when(javaMailSender.createMimeMessage()).thenReturn(mockMimeMessage);
         doThrow(new MessagingException())
                 .when(mockMimeMessage).setContent(anyString(), anyString());
 
-        verificationCodeEmailSender.sendEmail(TEST_EMAIL_SUBJECT, TEST_VERIFICATION_CODE_1, REVI_EMAIL_VALUE);
-
+        assertThatThrownBy(() -> verificationCodeEmailSender.sendEmail(TEST_EMAIL_SUBJECT, TEST_VERIFICATION_CODE_1, REVI_EMAIL_VALUE))
+                .isExactlyInstanceOf(EmailException.MimeSettingError.class);
         verify(javaMailSender, never()).send(any(MimeMessage.class));
+    }
+
+    @Test
+    @DisplayName("이메일 인증코드 발송에 실패하면 커스텀 예외가 발생한다.")
+    void fail2() {
+        MimeMessage mockMessage = new MimeMessage((Session) null);
+        when(javaMailSender.createMimeMessage()).thenReturn(mockMessage);
+        doThrow(new MailSendException("msg")).when(javaMailSender).send(mockMessage);
+
+        assertThatThrownBy(() -> verificationCodeEmailSender.sendEmail(TEST_EMAIL_SUBJECT, TEST_VERIFICATION_CODE_1, REVI_EMAIL_VALUE))
+                .isExactlyInstanceOf(EmailException.SendError.class);
     }
 }
