292974dfdabbb33cb7c5aac82c0bdac57d5be738
Revi1337 <david122123@gmail.com>
2026-01-13 00:34:01 +0900

    feat: 범용 Discord 알림 클라이언트 구현
    - 특정 도메인에 종속되지 않도록 webhookEndpoint를 파라미터로 분리
    - 다양한 파일 형식 지원 (MultipartFile, byte[])
    - Multipart/form-data 기반의 Discord API 규격(payload_json) 반영
    - 전송 실패 시 로깅
    
    Reference
    - https://discohook.org/
    - https://github.com/aeristhy/Discord-Webhook
    - https://birdie0.github.io/discord-webhooks-guide/structure/embed/footer.html

---
+++ src/main/java/revi1337/onsquad/notification/infrastructure/discord/NamedByteArrayResource.java
@@ -0,0 +1,18 @@
+package revi1337.onsquad.notification.infrastructure.discord;
+
+import org.springframework.core.io.ByteArrayResource;
+
+public class NamedByteArrayResource extends ByteArrayResource {
+
+    private final String fileName;
+
+    public NamedByteArrayResource(String fileName, byte[] byteArray) {
+        super(byteArray);
+        this.fileName = fileName;
+    }
+
+    @Override
+    public String getFilename() {
+        return this.fileName;
+    }
+}
