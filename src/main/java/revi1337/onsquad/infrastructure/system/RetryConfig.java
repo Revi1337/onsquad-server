326b1dd44f6a40325b64611d133f2383cb33edad
Revi1337 <david122123@gmail.com>
2026-01-15 02:04:03 +0900

    config: spring retry 의존성 추가

---
+++ src/main/java/revi1337/onsquad/infrastructure/system/RetryConfig.java
@@ -0,0 +1,10 @@
+package revi1337.onsquad.infrastructure.system;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+    
+}
