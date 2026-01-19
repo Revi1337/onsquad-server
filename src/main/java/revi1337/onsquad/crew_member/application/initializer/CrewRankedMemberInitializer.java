6ab104993249d08394861f5918302436cec282e3
Revi1337 <david122123@gmail.com>
2026-01-20 04:51:01 +0900

    refactor: 크루 랭커 캐시 초기화 키 패턴을 KeyMapper 참조로 변경

--- src/main/java/revi1337/onsquad/crew_member/application/initializer/CrewRankedMemberInitializer.java
+++ src/main/java/revi1337/onsquad/crew_member/application/initializer/CrewRankedMemberInitializer.java
@@ -1,30 +1,23 @@
 package revi1337.onsquad.crew_member.application.initializer;
 
 import java.util.List;
-import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import revi1337.onsquad.common.constant.CacheConst.CacheFormat;
+import revi1337.onsquad.crew_member.application.leaderboard.CrewLeaderboardKeyMapper;
 import revi1337.onsquad.infrastructure.storage.redis.RedisCacheEvictor;
 
 @Slf4j
 @RequiredArgsConstructor
 public class CrewRankedMemberInitializer {
 
-    private static final String CURRENT_CREW_RANKED_PATTERN = "crew:*:rank-members:current";
-    private static final String LAST_WEEK_CREW_RANKED_PATTERN = "crew:*:rank-members:last-week";
-
     private final StringRedisTemplate stringRedisTemplate;
 
     @EventListener(ApplicationReadyEvent.class)
     public void onApplicationEvent() {
-        List<String> patterns = Stream.of(CURRENT_CREW_RANKED_PATTERN, LAST_WEEK_CREW_RANKED_PATTERN)
-                .map(pattern -> String.format(CacheFormat.SIMPLE, pattern))
-                .toList();
-
+        List<String> patterns = List.of(CrewLeaderboardKeyMapper.getLeaderboardPattern(), CrewLeaderboardKeyMapper.getPreviousLeaderboardPattern());
         RedisCacheEvictor.scanKeysAndUnlink(stringRedisTemplate, patterns);
         log.info("[Cache-Evict] Initializing Crew ranking caches on startup. Patterns: {}", patterns);
     }
